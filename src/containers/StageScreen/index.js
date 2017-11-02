539b4b3f7dcbc6a727153f95cfdfbad23eefb41a
dsgoers <daniel.goers@cru.org>
2017-11-02 15:21:27 -0400

    Add some style to stage selection screen

--- src/containers/StageScreen/index.js
+++ src/containers/StageScreen/index.js
@@ -15,10 +15,14 @@ class StageScreen extends Component {
   render() {
     return (
       <Flex align="center" justify="center" value={1} style={styles.container}>
-        <Text>{this.props.firstName}, which stage best describes where you are on your journey?</Text>
-        <ScrollView horizontal={true}>
-          {this.renderStages()}
-        </ScrollView>
+        <Text style={{flex: 1}}></Text>
+        <View style={{flex: 2}}>
+          <Text style={{color: 'navy', fontSize: 18}}>{this.props.firstName}, which stage best describes where you are on your journey?</Text>
+          <ScrollView horizontal={true}>
+            {this.renderStages()}
+          </ScrollView>
+        </View>
+        <Text style={{flex: 1}}></Text>
       </Flex>
     );
   }
@@ -26,10 +30,11 @@ class StageScreen extends Component {
   renderStages() {
     if (this.props.stages) {
       return this.props.stages.map(stage =>
-        <View key={stage.id}>
-          <Text>{stage.name}</Text>
-          <Text>{stage.description}</Text>
+        <View key={stage.id} style={{justifyContent: 'space-between', backgroundColor: 'white', width: 250, marginLeft: 20, marginRight: 20}}>
+          <Text style={{fontSize: 42, fontWeight: 'bold', color: 'navy'}}>{stage.name}</Text>
+          <Text style={{fontSize: 16, color: 'navy'}}>{stage.description}</Text>
           <Button
+            style={{backgroundColor: 'navy'}}
             onPress={() => console.log('stage selected')}
             text="I AM HERE"
           />
