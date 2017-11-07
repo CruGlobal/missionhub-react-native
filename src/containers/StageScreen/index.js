1d8f5a679085aaa83b755461804d009f3fa846b5
dsgoers <daniel.goers@cru.org>
2017-11-07 11:55:39 -0500

    Set some colors

--- src/containers/StageScreen/index.js
+++ src/containers/StageScreen/index.js
@@ -9,6 +9,8 @@ import styles from './styles';
 import {Flex, Text, Button} from '../../components/common';
 
 class StageScreen extends Component {
+  color = 'rgba(0, 45, 6, 1)'
+
   componentWillMount() {
     this.props.dispatch(getStages());
   }
@@ -25,7 +27,7 @@ class StageScreen extends Component {
           <Button text="Back" onPress={() => this.props.dispatch(navigateBack())} />
         </View>
         <View style={{flex: 2}}>
-          <Text style={{color: 'navy', fontSize: 18}}>{this.props.firstName}, which stage best describes where you are on your journey?</Text>
+          <Text style={{color: this.color, fontSize: 18}}>{this.props.firstName}, which stage best describes where you are on your journey?</Text>
           <ScrollView horizontal={true}>
             {this.renderStages()}
           </ScrollView>
@@ -39,10 +41,10 @@ class StageScreen extends Component {
     if (this.props.stages) {
       return this.props.stages.map(stage =>
         <View key={stage.id} style={{justifyContent: 'space-between', backgroundColor: 'white', width: 250, marginLeft: 20, marginRight: 20}}>
-          <Text style={{fontSize: 42, fontWeight: 'bold', color: 'navy'}}>{stage.name}</Text>
-          <Text style={{fontSize: 16, color: 'navy'}}>{stage.description}</Text>
+          <Text style={{fontSize: 42, fontWeight: 'bold', color: this.color}}>{stage.name}</Text>
+          <Text style={{fontSize: 16, color: this.color}}>{stage.description}</Text>
           <Button
-            style={{backgroundColor: 'navy'}}
+            style={{backgroundColor: this.color}}
             onPress={() => this.setStage(stage.id)}
             text="I AM HERE"
           />
