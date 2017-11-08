e6e04c7b38fbbace32105e8f0f1d371f16816d10
dsgoers <daniel.goers@cru.org>
2017-11-08 10:31:24 -0500

    Add logo to Login screen

--- src/containers/LoginScreen/index.js
+++ src/containers/LoginScreen/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {View} from 'react-native';
+import {View, Image} from 'react-native';
 
 import {login, firstTime} from '../../actions/auth';
 import styles from './styles';
-import {Flex, Text, Button} from '../../components/common';
+import {Text, Button} from '../../components/common';
 import {navigatePush} from '../../actions/navigation';
 import projectStyles from '../../projectStyles';
 
@@ -36,10 +36,10 @@ class LoginScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex: 3}} />
+        <View style={{flex: 1}} />
         <View style={{flex: 4, justifyContent: 'space-between'}}>
           <View style={{alignItems: 'center'}}>
-            <Text>MissionHub</Text>
+            <Image source={require('../../../assets/images/missionhub_logo_circle.png')} />
             <Text style={projectStyles.primaryTextStyle}>Grow closer to God.</Text>
             <Text style={projectStyles.primaryTextStyle}>Help others experience Him.</Text>
           </View>
@@ -47,18 +47,20 @@ class LoginScreen extends Component {
             <Button
               onPress={() => console.log('join')}
               text="SIGN UP WITH FACEBOOK"
-              style={{alignItems: 'center'}}
+              style={{alignItems: 'center', backgroundColor: '#005A7F'}}
               buttonTextStyle={this.buttonTextStyle}
             />
-            <Button
-              onPress={() => this.tryItNow()}
-              text="TRY IT NOW"
-              style={{alignItems: 'center'}}
-              buttonTextStyle={this.buttonTextStyle}
-            />
+            <View style={{paddingTop: 10}}>
+              <Button
+                onPress={() => this.tryItNow()}
+                text="TRY IT NOW"
+                style={{alignItems: 'center'}}
+                buttonTextStyle={this.buttonTextStyle}
+              />
+            </View>
           </View>
         </View>
-        <View style={{flex: 3, justifyContent: 'flex-end'}}>
+        <View style={{flex: 1, justifyContent: 'flex-end'}}>
           <Button
             style={{alignItems: 'center'}}
             onPress={() => this.login()}
