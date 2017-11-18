3058f2041e13dbae98c05761b641e7192d67d138
Ben Gauthier <bengauthier@bens-mbp.lan>
2017-11-17 21:20:45 -0500

    setup flex on welcome screen

--- src/containers/WelcomeScreen/index.js
+++ src/containers/WelcomeScreen/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { navigatePush } from '../../actions/navigation';
+import theme from '../../theme';
 
 import styles from './styles';
-import { Flex, Text, Button } from '../../components/common';
+import { Flex, Text, Button, BackButton } from '../../components/common';
 
 class WelcomeScreen extends Component {
   navigateToNext() {
@@ -17,13 +18,17 @@ class WelcomeScreen extends Component {
   render() {
     return (
       <Flex align="center" justify="center" value={1} style={styles.container}>
-        <Text type="header" style={styles.headerText}>welcome!</Text>
-        <Text style={styles.descriptionText}>Growing closer to God involves helping others experience Him. MissionHub joins you in that journey by suggesting steps of faith to take with others.</Text>
-        <Flex style={{position: 'absolute', bottom: 0, left: 0, right: 0}}>
+        <BackButton />
+        <Flex value={4} align="center" justify="center">
+          <Text type="header" style={styles.headerText}>welcome!</Text>
+          <Text style={styles.descriptionText}>Growing closer to God involves helping others experience Him. MissionHub joins you in that journey by suggesting steps of faith to take with others.</Text>
+        </Flex>
+        <Flex value={1} align="stretch" justify="end">
           <Button
             type="secondary"
             onPress={() => this.navigateToNext()}
             text="OK"
+            style={{width: theme.fullWidth}}
           />
         </Flex>
       </Flex>
