ecf8b9df68c3ed0fa7a18eb231b95323c1051996
dsgoers <daniel.goers@cru.org>
2017-11-20 15:21:08 -0500

    Show the back button a little lower on iPhone X

--- src/containers/BackButton/styles.js
+++ src/containers/BackButton/styles.js
@@ -1,6 +1,9 @@
 
 import { StyleSheet } from 'react-native';
 import theme from '../../theme';
+import DeviceInfo from 'react-native-device-info';
+
+const margin = 20;
 
 export default StyleSheet.create({
   button: {
@@ -11,7 +14,8 @@ export default StyleSheet.create({
     marginRight: 10,
   },
   icon: {
-    margin: 20,
+    margin: margin,
+    marginTop: (DeviceInfo.getModel() === 'iPhone X' ? 50 : margin),
   },
   buttonText: {
     color: theme.primaryColor,
