c199ce32984b7d2119d9d222b11350b049859e7a
Ben Gauthier <bengauthier@Bens-MacBook-Pro.local>
2017-11-29 17:19:18 -0500

    create icon button and style header properties

---
+++ src/components/IconButton/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {Button, Icon} from '../common';
+import styles from './styles';
+// import theme from '../../theme';
+
+
+export default class IconButton extends Component {
+  render() {
+    const { name, type, style = {}, ...rest } = this.props;
+
+    return (
+      <Button
+        type="transparent"
+        {...rest}
+      >
+        <Icon name={name} type={type} style={[styles.iconWrap, style]} {...rest} />
+      </Button>
+    );
+  }
+}
+
+IconButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  style: PropTypes.object,
+};
--- src/containers/StepsScreen/index.js
+++ src/containers/StepsScreen/index.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 
-import { logout } from '../../actions/auth';
+// import { logout } from '../../actions/auth';
 import { navigatePush } from '../../actions/navigation';
 
 import styles from './styles';
-import { Flex, Text, Button } from '../../components/common';
-import Header, { HeaderIcon } from '../Header';
+import { Flex, Text, Button, IconButton } from '../../components/common';
+import Header from '../Header';
 
 class StepsScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Header
-          left={<HeaderIcon
-            onPress={() => this.props.dispatch(logout())}
-            icon="home"
-          />}
-          right={<HeaderIcon
-            onPress={() => this.props.dispatch(logout())}
-            icon="more-vert"
-          />}
+          left={
+            <IconButton name="stepsIcon" type="MissionHub" onPress={()=> LOG('pressed')} />
+          }
+          right={
+            <IconButton name="stepsIcon" type="MissionHub" onPress={()=> LOG('pressed')} />
+          }
           title="Steps"
         />
         <Flex align="center" justify="center" value={1} style={styles.container}>
