74e01a56d79ca9cc6b4babad7c30bbc2decf6638
Bryan Eaton <bryaneaton13@gmail.com>
2017-12-14 17:39:27 -0500

    Add tests and fix broken ones.

--- __tests__/StepsList.js
+++ __tests__/StepsList.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 // Note: test renderer must be required after react-native.
 import StepsList from '../src/components/StepsList';
-import {testSnapshot} from '../testUtils';
+import { testSnapshot } from '../testUtils';
 
 const items = [
-  {body: 'I feel great', selected: true},
-  {body: 'I feel wonderful', selected: false},
+  { id: '1', body: 'I feel great', selected: true },
+  { id: '2', body: 'I feel wonderful', selected: false },
 ];
 
 it('renders correctly', () => {
   testSnapshot(
-    <StepsList items={items} />
+    <StepsList items={items} onSelectStep={() => {}} onCreateStep={() => {}} />
   );
 });
--- src/containers/SearchPeopleScreen/index.js
+++ src/containers/SearchPeopleScreen/index.js
@@ -16,6 +16,7 @@ export class SearchPeopleScreen extends Component {
 
   constructor(props) {
     super(props);
+    
     this.state = {
       text: '',
       results: [],
