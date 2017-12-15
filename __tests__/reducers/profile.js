b1fe0e9b87154450319a74fc05edc2a6de1ebc14
dsgoers <daniel.goers@cru.org>
2017-12-15 13:39:00 -0500

    Add tests for profile reducer

---
+++ __tests__/reducers/profile.js
@@ -0,0 +1,29 @@
+import profile from '../../src/reducers/profile';
+import {REQUESTS} from '../../src/actions/api';
+
+const testNameSaved = (type) => {
+  const firstName = 'Roger';
+  const lastName = 'Goers';
+
+  const state = profile(
+    {},
+    {
+      type: type,
+      results: {
+        first_name: firstName,
+        last_name: lastName,
+      },
+    }
+  );
+
+  expect(state.firstName).toBe(firstName);
+  expect(state.lastName).toBe(lastName);
+};
+
+it('saves first and last name after creating person', () => {
+  testNameSaved(REQUESTS.CREATE_MY_PERSON.SUCCESS);
+});
+
+it('saves first and last name after logging in with ticket', () => {
+  testNameSaved(REQUESTS.TICKET_LOGIN.SUCCESS);
+});
