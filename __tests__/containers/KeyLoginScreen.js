437275e92aace139f7eb660033eb1ed267c83c9f
dsgoers <daniel.goers@cru.org>
2018-01-18 08:35:46 -0500

    Add some more login button tests

--- __tests__/containers/KeyLoginScreen.js
+++ __tests__/containers/KeyLoginScreen.js
@@ -9,9 +9,9 @@ import { createMockStore, testSnapshot } from '../../testUtils';
 import { Provider } from 'react-redux';
 import * as auth from '../../src/actions/auth';
 import * as navigation from '../../src/actions/navigation';
+import * as people from '../../src/actions/people';
 
 let store;
-let screen;
 
 jest.mock('react-native-device-info');
 jest.mock('../../src/actions/auth');
@@ -31,28 +31,58 @@ it('renders correctly', () => {
 });
 
 describe('when login button is clicked', () => {
-  const mockNavResult = 'test';
   let user;
+  let person;
+  let screen;
+
+  const click = () => screen.dive().dive().dive().find('Button').simulate('press');
+
+  const setupUserAndPerson = (userStage, personStage) => {
+    user = { pathway_stage_id: userStage };
+    person = {
+      reverse_contact_assignments: [ { pathway_stage_id: personStage }],
+    };
+  };
 
   beforeEach(() => {
-    user =  { pathway_stage_id: null };
-
-    navigation.navigatePush = jest.fn().mockReturnValue(mockNavResult);
-
-    auth.keyLogin = jest.fn().mockReturnValue(Promise.resolve({
-      findAll: () => [user],
-    }));
-  });
-
-  it('user navigates to GetStarted screen if stage is not set', async() => {
     screen = shallow(
       <KeyLoginScreen />,
       { context: { store: store } }
     );
 
-    await screen.dive().dive().dive().find('Button').simulate('press');
+    navigation.navigatePush = (screen) => screen;
 
-    expect(store.dispatch).toHaveBeenLastCalledWith(mockNavResult);
+    auth.keyLogin = jest.fn().mockReturnValue(Promise.resolve({
+      findAll: () => [user],
+    }));
+
+    people.getPeopleList = jest.fn().mockReturnValue(Promise.resolve({
+      findAll: () => [person],
+    }));
+  });
+
+  it('user navigates to GetStarted screen if stage is not set', async() => {
+    setupUserAndPerson(null, 3);
+
+    await click();
+
+    expect(store.dispatch).toHaveBeenLastCalledWith('GetStarted');
+  });
+
+  it('user navigates to MainTabs if stage is set and has a contact with stage set', async() => {
+    setupUserAndPerson(5, 3);
+
+    await await click();
+
+    expect(store.dispatch).toHaveBeenLastCalledWith('MainTabs');
+  });
+
+  it('user navigates to AddSomeone screen if stage is set but does not have a contact with stage set', async() => {
+    setupUserAndPerson(5, null);
+
+    await await click();
+
+    expect(store.dispatch).toHaveBeenLastCalledWith('AddSomeone');
   });
 });
 
