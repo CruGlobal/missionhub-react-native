871383523472b798c01170bdac660296a4fd7b83
dsgoers <daniel.goers@cru.org>
2018-01-18 10:00:52 -0500

    Add a comment

--- src/components/BaseScreen.js
+++ src/components/BaseScreen.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { trackState } from '../actions/analytics';
 
+//NOTE: only classes with connect() should extend this
 class BaseScreen extends Component {
   componentDidMount() {
     this.props.dispatch(trackState(this.constructor.name));
