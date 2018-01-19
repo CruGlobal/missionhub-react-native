4708f04516cb59af2833fc99283a16ad6eed4d18
dsgoers <daniel.goers@cru.org>
2018-01-19 10:31:30 -0500

    Add another test for people action creator

--- __tests__/actions/people.js
+++ __tests__/actions/people.js
@@ -1,4 +1,4 @@
-import { getMe } from '../../src/actions/people';
+import { getMe, getPeopleList } from '../../src/actions/people';
 import * as api from '../../src/actions/api';
 import { REQUESTS } from '../../src/actions/api';
 
@@ -6,17 +6,40 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
 const mockStore = configureStore([thunk]);
-const store = mockStore();
+let store;
+
+const mockApi = (result, expectedRequestObj, expectedQuery) => {
+  api.default = jest.fn().mockImplementation((requestObj, query) =>
+    requestObj === expectedRequestObj && JSON.stringify(query) === JSON.stringify(expectedQuery) ? result : null
+  );
+};
+
+beforeEach(() => store = mockStore());
 
 describe('get me', () => {
   const action = { type: 'got me' };
 
-  api.default = jest.fn().mockImplementation(
-    (requestObj) => requestObj === REQUESTS.GET_ME ? action : null
-  );
+  beforeEach(() => mockApi(action, REQUESTS.GET_ME));
 
   it('should get me', () => {
     store.dispatch(getMe());
+
+    expect(store.getActions()[0]).toBe(action);
+  });
+});
+
+describe('get people list', () => {
+  const expectedQuery = {
+    filters: {
+      assigned_tos: 'me',
+    },
+  };
+  const action = { type: 'got people' };
+
+  beforeEach(() => mockApi(action, REQUESTS.GET_PEOPLE_LIST, expectedQuery));
+
+  it('should get people list', () => {
+    store.dispatch(getPeopleList());
 
     expect(store.getActions()[0]).toBe(action);
   });
