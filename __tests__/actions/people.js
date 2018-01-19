47eb7d4a0676892b157b9f8a762f5f51f0807a7e
dsgoers <daniel.goers@cru.org>
2018-01-19 11:16:45 -0500

    Add a test for getting Casey people

--- __tests__/actions/people.js
+++ __tests__/actions/people.js
@@ -1,11 +1,11 @@
-import { getMe, getPeopleList } from '../../src/actions/people';
+import { getMe, getPeopleList, getPeopleWithOrgSections } from '../../src/actions/people';
 import * as api from '../../src/actions/api';
 import { REQUESTS } from '../../src/actions/api';
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { JsonApiDataStore } from 'jsonapi-datastore';
 
-const mockStore = configureStore([thunk]);
 let store;
 
 const mockApi = (result, expectedRequestObj, expectedQuery) => {
@@ -14,7 +14,7 @@ const mockApi = (result, expectedRequestObj, expectedQuery) => {
   );
 };
 
-beforeEach(() => store = mockStore());
+beforeEach(() => store = configureStore([thunk])());
 
 describe('get me', () => {
   const action = { type: 'got me' };
@@ -42,5 +42,44 @@ describe('get people list', () => {
     store.dispatch(getPeopleList());
 
     expect(store.getActions()[0]).toBe(action);
+  });
+});
+
+describe('get people with org sections', () => {
+  const jsonApiStore = new JsonApiDataStore();
+
+  const expectedQuery = {
+    filters: {
+      assigned_tos: 'me',
+    },
+    includes: 'organizational_permission,organization',
+  };
+
+  const mockApiReturnValue = (dispatch) => {
+    return dispatch(() => Promise.resolve(jsonApiStore));
+  };
+
+  beforeEach(() => {
+    jsonApiStore.sync(
+      {
+        data: [
+          {
+            type: 'person',
+          },
+        ],
+      }
+    );
+
+    mockApi(mockApiReturnValue, REQUESTS.GET_PEOPLE_LIST, expectedQuery);
+  });
+
+  it('should get people with org sections', () => {
+    store = configureStore([thunk])(
+      { auth: { hasMinistries: false } }
+    );
+
+    return store.dispatch(getPeopleWithOrgSections()).then((result) => {
+      expect(result).toBe(jsonApiStore);
+    });
   });
 });
