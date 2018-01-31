21edd19881bb6075456e63354d8f05781bfaf5d3
Robert Eldredge <robert.eldredge@cru.org>
2018-01-31 13:48:27 -0500

    AddSomeoneScreen Update

--- __tests__/containers/AddSomeoneScreen.js
+++ __tests__/containers/AddSomeoneScreen.js
@@ -3,21 +3,16 @@ import React from 'react';
 
 // Note: test renderer must be required after react-native.
 import AddSomeoneScreen from '../../src/containers/AddSomeoneScreen';
-import { Provider } from 'react-redux';
 import { createMockStore } from '../../testUtils/index';
 import { testSnapshotShallow } from '../../testUtils';
 
-const mockState = {
-  notifications: { hasAsked: true },
-};
-const store = createMockStore(mockState);
+const store = createMockStore();
 
 jest.mock('react-native-device-info');
 
 it('renders correctly', () => {
   testSnapshotShallow(
-    <Provider store={store}>
-      <AddSomeoneScreen />
-    </Provider>
+    <AddSomeoneScreen />,
+    store
   );
 });
