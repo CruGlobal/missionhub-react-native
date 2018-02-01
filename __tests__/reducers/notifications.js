17362547bac0aed2559eddcb1d9cc6b2e902a0b0
Ben Gauthier <bengauthier@bens-mbp.lan>
2018-02-01 10:26:23 -0500

    add tests

---
+++ __tests__/reducers/notifications.js
@@ -0,0 +1,46 @@
+import notifications from '../../src/reducers/notifications';
+import {
+  PUSH_NOTIFICATION_ASKED,
+  PUSH_NOTIFICATION_SHOULD_ASK,
+  PUSH_NOTIFICATION_SET_TOKEN,
+  PUSH_NOTIFICATION_REMINDER,
+} from '../../src/constants';
+import { REQUESTS } from '../../src/actions/api';
+
+it('updates shouldAsk', () => {
+  const state = notifications({}, {
+    type: PUSH_NOTIFICATION_SHOULD_ASK,
+    bool: true,
+  });
+  expect(state.shouldAsk).toBe(true);
+});
+
+it('updates hasAsked', () => {
+  const state = notifications({}, {
+    type: PUSH_NOTIFICATION_ASKED,
+  });
+  expect(state.hasAsked).toBe(true);
+});
+
+it('updates showReminder', () => {
+  const state = notifications({}, {
+    type: PUSH_NOTIFICATION_REMINDER,
+    bool: false,
+  });
+  expect(state.showReminder).toBe(false);
+});
+
+it('updates set token', () => {
+  const state = notifications({}, {
+    type: PUSH_NOTIFICATION_SET_TOKEN,
+    token: '123',
+  });
+  expect(state.token).toBe('123');
+});
+
+it('updates set token', () => {
+  const state = notifications({}, {
+    type: REQUESTS.SET_PUSH_TOKEN.SUCCESS,
+  });
+  expect(state.isRegistered).toBe(true);
+});
