6da31932f903714668ffcc16bbd67fb284811044
dsgoers <daniel.goers@cru.org>
2018-02-09 11:40:10 -0500

    Refactor again

--- __tests__/middleware/tracking.js
+++ __tests__/middleware/tracking.js
@@ -11,12 +11,19 @@ const mockStore = configureStore([ tracking ]);
 let store;
 
 const routeName = 'test route';
-let screenName = 'test : middleware';
 let navigationAction;
 
 const back = { type: 'Navigation/BACK' };
 
 const trackStateResult = { type: 'tracked state' };
+
+const test = (expectedScreenname) => {
+  mockFnWithParams(analytics, 'trackState', trackStateResult, expectedScreenname);
+
+  store.dispatch(navigationAction);
+
+  expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+};
 
 describe('navigate forward', () => {
   beforeEach(() => {
@@ -24,25 +31,19 @@ describe('navigate forward', () => {
   });
 
   it('tracks screenname when navigating', () => {
-    screenName = 'test : forward';
+    const screenName = 'test : forward';
     navigationAction = { type: NAVIGATE_FORWARD, routeName: routeName };
     trackableScreens[routeName] = { name: screenName };
-    mockFnWithParams(analytics, 'trackState', trackStateResult, screenName);
 
-    store.dispatch(navigationAction);
-
-    expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+    test(screenName);
   });
 
   describe('to contact screen', () => {
     it('tracks person steps', () => {
       store = mockStore({ auth: { personId: 2 } });
       navigationAction = { type: NAVIGATE_FORWARD, routeName: CONTACT_SCREEN, params: { person: { id: 1 } } };
-      mockFnWithParams(analytics, 'trackState', trackStateResult, PERSON_STEPS);
 
-      store.dispatch(navigationAction);
-
-      expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+      test(PERSON_STEPS);
     });
 
 
@@ -50,11 +51,8 @@ describe('navigate forward', () => {
       const id = 3;
       store = mockStore({ auth: { personId: id } });
       navigationAction = { type: NAVIGATE_FORWARD, routeName: CONTACT_SCREEN, params: { person: { id: id } } };
-      mockFnWithParams(analytics, 'trackState', trackStateResult, SELF_STEPS);
 
-      store.dispatch(navigationAction);
-
-      expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+      test(SELF_STEPS);
     });
   });
 
@@ -65,30 +63,21 @@ describe('navigate forward', () => {
 
     it('tracks main menu drawer', () => {
       navigationAction.params = { drawer: MAIN_MENU_DRAWER };
-      mockFnWithParams(analytics, 'trackState', trackStateResult, 'menu : menu');
 
-      store.dispatch(navigationAction);
-
-      expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+      test('menu : menu');
     });
 
     describe('contact drawer', () => {
       it('tracks self menu', () => {
         navigationAction.params = { drawer: CONTACT_MENU_DRAWER, isCurrentUser: false };
-        mockFnWithParams(analytics, 'trackState', trackStateResult, 'people : person : menu : menu');
 
-        store.dispatch(navigationAction);
-
-        expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+        test('people : person : menu : menu');
       });
 
       it('tracks person menu', () => {
         navigationAction.params = { drawer: CONTACT_MENU_DRAWER, isCurrentUser: true };
-        mockFnWithParams(analytics, 'trackState', trackStateResult, 'people : self : menu : menu');
 
-        store.dispatch(navigationAction);
-
-        expect(store.getActions()).toEqual([ navigationAction, trackStateResult ]);
+        test('people : self : menu : menu');
       });
     });
   });
