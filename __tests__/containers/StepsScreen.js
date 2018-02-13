f891e602e4e9e13326d86de2caf6ccd25ecafba6
Robert Eldredge <robert.eldredge@cru.org>
2018-02-13 17:05:53 -0500

    Begin test for StepsScreen

--- __tests__/containers/StepsScreen.js
+++ __tests__/containers/StepsScreen.js
@@ -1,11 +1,14 @@
-import 'react-native';
+import { ScrollView } from 'react-native';
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
 
 // Note: test renderer must be required after react-native.
 import { Provider } from 'react-redux';
 import { createMockStore } from '../../testUtils/index';
 import StepsScreen from '../../src/containers/StepsScreen';
 import { testSnapshot } from '../../testUtils';
+import theme from '../../src/theme';
+import Adapter from 'enzyme-adapter-react-16/build/index';
 
 const mockState = {
   auth: {
@@ -51,3 +54,22 @@ it('renders correctly as Jean', () => {
     </Provider>
   );
 });
+
+
+describe('Background color changes with scrolling', () => {
+  Enzyme.configure({ adapter: new Adapter() });
+
+  const screen = shallow(
+    <StepsScreen />,
+    { context: { store } },
+  );
+
+  let component = screen.dive().dive().dive();
+
+  it('Background is white', () => {
+    const color = component.find(ScrollView).props().styles.find('backgroundColor');
+
+    expect(color).toBe(theme.white);
+  });
+});
+
