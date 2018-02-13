1fdbc2de33e374655aacb7ed1603e079769d3f2b
Scotty Waggoner <ozzieorca@gmail.com>
2018-02-13 13:58:57 -0800

    Check if organization is defined. Get org from steps screen

--- src/components/ContactSideMenu/index.js
+++ src/components/ContactSideMenu/index.js
@@ -44,13 +44,13 @@ export class ContactSideMenu extends Component {
   }
 
   render() {
-    const { t, dispatch, myId, stages } = this.props;
+    const { t, dispatch, myId, stages, organization } = this.props;
     const { isJean, personIsCurrentUser, person } = this.props.visiblePersonInfo;
 
     const isCaseyNotMe = !isJean && !personIsCurrentUser;
     const isJeanNotMe = isJean && !personIsCurrentUser;
 
-    const orgPermission = person.organizational_permissions && person.organizational_permissions.find((orgPermission) => orgPermission.organization_id === this.props.navigation.state.params.organization.id);
+    const orgPermission = organization && person.organizational_permissions && person.organizational_permissions.find((orgPermission) => orgPermission.organization_id === organization.id);
 
     const canEditFollowupStatus = isJeanNotMe && orgPermission;
 
@@ -100,7 +100,8 @@ export class ContactSideMenu extends Component {
   }
 }
 
-const mapStateToProps = ({ profile, auth, stages }) => ({
+const mapStateToProps = ({ profile, auth, stages }, { navigation }) => ({
+  ...(navigation.state.params || {}),
   myId: auth.personId,
   stages: stages.stages,
   visiblePersonInfo: profile.visiblePersonInfo || {},
