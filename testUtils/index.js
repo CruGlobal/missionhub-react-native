e4eb267b76579180e6a60d345c8704ae6d5a8e61
dsgoers <daniel.goers@cru.org>
2018-02-14 16:09:05 -0500

    Minor refactor

--- testUtils/index.js
+++ testUtils/index.js
@@ -47,15 +47,12 @@ export const testSnapshotShallow = (component, store) => {
 };
 
 export const mockFnWithParams = (obj, method, expectedReturn, ...expectedParams) => {
-  return obj[method] = jest.fn().mockImplementation(
-    (...actualParams) => JSON.stringify(expectedParams) === JSON.stringify(actualParams) ? expectedReturn : undefined
-  );
+  return mockFnWithParamsMultiple(obj, method, { expectedReturn: expectedReturn, expectedParams: expectedParams });
 };
 
 export const mockFnWithParamsMultiple = (obj, method, ...mockValuesList) => {
   return obj[method] = jest.fn().mockImplementation(
     (...actualParams) => {
-
       const mock = mockValuesList.find((mockValue) => JSON.stringify(mockValue.expectedParams) === JSON.stringify(actualParams));
       return mock ? mock.expectedReturn : undefined;
     }
