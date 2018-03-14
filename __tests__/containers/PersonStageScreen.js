ac091aa8fde19e6715f24dfc8ff68d82b501d042
dsgoers <daniel.goers@cru.org>
2018-03-14 15:23:14 -0400

    Add another test

--- __tests__/containers/PersonStageScreen.js
+++ __tests__/containers/PersonStageScreen.js
@@ -5,10 +5,15 @@ import Adapter from 'enzyme-adapter-react-16';
 import { Provider } from 'react-redux';
 
 // Note: test renderer must be required after react-native.
-import PersonStageScreen from '../../src/containers/PersonStageScreen';
+import PersonStageScreen,  from '../../src/containers/PersonStageScreen';
 import { testSnapshot, createMockNavState, createMockStore } from '../../testUtils';
 import * as navigation from '../../src/actions/navigation';
 import * as selectStage from '../../src/actions/selectStage';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as analytics from '../../src/actions/analytics';
+import { navigatePush } from '../../src/actions/navigation';
+import { PERSON_SELECT_STEP_SCREEN } from '../../src/containers/PersonSelectStepScreen';
 
 const mockState = {
   personProfile: {
@@ -71,7 +76,7 @@ describe('person stage screen methods', () => {
   });
 
   it('runs select stage', () => {
-    
+
     selectStage.updateUserStage = jest.fn();
 
     component.handleSelectStage(mockStage, false);
@@ -112,23 +117,41 @@ describe('person stage screen methods with add contact flow', () => {
   });
 
   it('runs handle navigate', () => {
-
     component.celebrateAndFinish = jest.fn();
 
     component.handleNavigate();
     expect(component.celebrateAndFinish).toHaveBeenCalledTimes(1);
   });
 
-  it('runs update stage', () => {
-    
-    selectStage.updateUserStage = jest.fn();
+  it('runs update stage', async() => {
+    const trackStateResult = { type: 'tracked state' };
+    const mockStore = configureStore([ thunk ])(mockState);
+    const screen = shallow(
+      <PersonStageScreen
+        navigation={createMockNavState({
+          onCompleteCelebration: mockComplete,
+          addingContactFlow: true,
+          name: 'Test',
+          contactId: '123',
+          currentStage: '2',
+          contactAssignmentId: '333',
+          section: 'section',
+          subsection: 'subsection',
+        })}
+      />,
+      { context: { store: mockStore } },
+    );
+    component = screen.dive().dive().dive().instance();
+    selectStage.updateUserStage = () => () => Promise.resolve();
+    analytics.trackState = () => (trackStateResult);
+    navigation.navigatePush = jest.fn();
 
-    component.handleSelectStage(mockStage, false);
-    expect(selectStage.updateUserStage).toHaveBeenCalledTimes(1);
+    await component.handleSelectStage(mockStage, false);
+
+    expect(navigatePush).toHaveBeenCalledWith(PERSON_SELECT_STEP_SCREEN, expect.anything());
   });
-  
-  it('runs celebrate and finish with on complete', () => {
 
+  it('runs celebrate and finish with on complete', () => {
     navigation.navigatePush = jest.fn();
 
     component.celebrateAndFinish();
@@ -162,7 +185,7 @@ describe('person stage screen methods', () => {
   });
 
   it('runs select stage with active', () => {
-    
+
     component.handleSelectStage(mockStage, true);
     expect(mockComplete).toHaveBeenCalledTimes(1);
   });
