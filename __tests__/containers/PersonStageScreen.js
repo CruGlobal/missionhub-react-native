90c2078bc186a152e33b56e05074182e9d80f6f4
dsgoers <daniel.goers@cru.org>
2018-03-14 15:51:29 -0400

    Refactor a test file

--- __tests__/containers/PersonStageScreen.js
+++ __tests__/containers/PersonStageScreen.js
@@ -1,12 +1,12 @@
 import 'react-native';
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Provider } from 'react-redux';
 
 // Note: test renderer must be required after react-native.
 import PersonStageScreen from '../../src/containers/PersonStageScreen';
-import { testSnapshot, createMockNavState, createMockStore } from '../../testUtils';
+import { testSnapshot, createMockNavState, createMockStore, renderShallow } from '../../testUtils';
 import * as navigation from '../../src/actions/navigation';
 import * as selectStage from '../../src/actions/selectStage';
 import configureStore from 'redux-mock-store';
@@ -15,6 +15,8 @@ import * as analytics from '../../src/actions/analytics';
 import { navigatePush } from '../../src/actions/navigation';
 import { PERSON_SELECT_STEP_SCREEN } from '../../src/containers/PersonSelectStepScreen';
 import { buildTrackingObj } from '../../src/utils/common';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const mockState = {
   personProfile: {
@@ -31,22 +33,42 @@ const mockStage = {
 };
 
 let store = createMockStore(mockState);
+const mockNavState = {
+  name: 'Test',
+  contactId: '123',
+  currentStage: '2',
+  contactAssignmentId: '333',
+  section: 'section',
+  subsection: 'subsection',
+};
 
 jest.mock('react-native-device-info');
+
+function buildScreen(mockNavState, store) {
+  const screen = renderShallow(
+    <PersonStageScreen
+      navigation={createMockNavState(mockNavState)}
+    />,
+    store
+  );
+
+  return screen.instance();
+}
+
+navigation.navigatePush = jest.fn();
+
+beforeEach(() => {
+  navigation.navigatePush.mockReset();
+});
 
 it('renders correctly', () => {
   testSnapshot(
     <Provider store={store}>
       <PersonStageScreen
         navigation={createMockNavState({
+          ...mockNavState,
           onComplete: jest.fn(),
-          name: 'Test',
-          contactId: '123',
-          currentStage: '2',
-          contactAssignmentId: '333',
           enableBackButton: true,
-          section: 'section',
-          subsection: 'subsection',
         })}
       />
     </Provider>
@@ -56,39 +78,25 @@ it('renders correctly', () => {
 describe('person stage screen methods', () => {
   let component;
   const mockComplete = jest.fn();
-  beforeEach(() => {
-    Enzyme.configure({ adapter: new Adapter() });
-    const screen = shallow(
-      <PersonStageScreen
-        navigation={createMockNavState({
-          onComplete: mockComplete,
-          name: 'Test',
-          contactId: '123',
-          currentStage: '2',
-          contactAssignmentId: '333',
-          section: 'section',
-          subsection: 'subsection',
-        })}
-      />,
-      { context: { store } },
-    );
 
-    component = screen.dive().dive().dive().instance();
+  beforeEach(() => {
+    component = buildScreen({
+      ...mockNavState,
+      onComplete: mockComplete,
+    }, store);
   });
 
   it('runs select stage', () => {
-
     selectStage.updateUserStage = jest.fn();
 
     component.handleSelectStage(mockStage, false);
+
     expect(selectStage.updateUserStage).toHaveBeenCalledTimes(1);
   });
 
   it('runs celebrate and finish', () => {
-
-    navigation.navigatePush = jest.fn();
-
     component.celebrateAndFinish();
+
     expect(navigation.navigatePush).toHaveBeenCalledTimes(1);
   });
 });
@@ -96,56 +104,34 @@ describe('person stage screen methods', () => {
 describe('person stage screen methods with add contact flow', () => {
   let component;
   const mockComplete = jest.fn();
-  beforeEach(() => {
-    Enzyme.configure({ adapter: new Adapter() });
-    const screen = shallow(
-      <PersonStageScreen
-        navigation={createMockNavState({
-          onCompleteCelebration: mockComplete,
-          addingContactFlow: true,
-          name: 'Test',
-          contactId: '123',
-          currentStage: '2',
-          contactAssignmentId: '333',
-          section: 'section',
-          subsection: 'subsection',
-        })}
-      />,
-      { context: { store } },
-    );
 
-    component = screen.dive().dive().dive().instance();
+  beforeEach(() => {
+    component = buildScreen({
+      onCompleteCelebration: mockComplete,
+      addingContactFlow: true,
+      ...mockNavState,
+    }, store);
   });
 
   it('runs handle navigate', () => {
     component.celebrateAndFinish = jest.fn();
 
     component.handleNavigate();
+
     expect(component.celebrateAndFinish).toHaveBeenCalledTimes(1);
   });
 
   it('runs update stage', async() => {
     const trackStateResult = { type: 'tracked state' };
-    const mockStore = configureStore([ thunk ])(mockState);
-    const screen = shallow(
-      <PersonStageScreen
-        navigation={createMockNavState({
-          onCompleteCelebration: mockComplete,
-          addingContactFlow: true,
-          name: 'Test',
-          contactId: '123',
-          currentStage: '2',
-          contactAssignmentId: '333',
-          section: 'section',
-          subsection: 'subsection',
-        })}
-      />,
-      { context: { store: mockStore } },
-    );
-    component = screen.dive().dive().dive().instance();
-    selectStage.updateUserStage = () => () => Promise.resolve();
     analytics.trackState = jest.fn(() => (trackStateResult));
-    navigation.navigatePush = jest.fn();
+
+    const mockStore = configureStore([ thunk ])(mockState);
+    component = buildScreen({
+      onCompleteCelebration: mockComplete,
+      addingContactFlow: true,
+      ...mockNavState,
+    }, mockStore);
+    selectStage.updateUserStage = () => () => Promise.resolve();
 
     await component.handleSelectStage(mockStage, false);
 
@@ -154,41 +140,27 @@ describe('person stage screen methods with add contact flow', () => {
   });
 
   it('runs celebrate and finish with on complete', () => {
-    navigation.navigatePush = jest.fn();
-
     component.celebrateAndFinish();
+
     expect(navigation.navigatePush).toHaveBeenCalledTimes(1);
   });
-
 });
 
 describe('person stage screen methods', () => {
   let component;
   const mockComplete = jest.fn();
-  beforeEach(() => {
-    Enzyme.configure({ adapter: new Adapter() });
-    const screen = shallow(
-      <PersonStageScreen
-        navigation={createMockNavState({
-          onComplete: mockComplete,
-          name: 'Test',
-          contactId: '123',
-          currentStage: '2',
-          contactAssignmentId: '333',
-          noNav: true,
-          section: 'section',
-          subsection: 'subsection',
-        })}
-      />,
-      { context: { store } },
-    );
 
-    component = screen.dive().dive().dive().instance();
+  beforeEach(() => {
+    component = buildScreen({
+      onComplete: mockComplete,
+      noNav: true,
+      ...mockNavState,
+    }, store);
   });
 
   it('runs select stage with active', () => {
-
     component.handleSelectStage(mockStage, true);
+
     expect(mockComplete).toHaveBeenCalledTimes(1);
   });
 });
