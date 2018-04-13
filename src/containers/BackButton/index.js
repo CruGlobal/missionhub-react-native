16a83c3366675100082f3cacf00fb7bb75c6721c
Robert Eldredge <robert.eldredge@cru.org>
2018-04-13 11:02:30 -0400

    BackButton styles

--- src/containers/BackButton/index.js
+++ src/containers/BackButton/index.js
@@ -13,11 +13,12 @@ export class BackButton extends Component {
   render() {
     const { dispatch, customNavigate, absolute } = this.props;
     return (
-      <Flex self="start" align="start" justify="center" style={absolute ? styles.absoluteTopLeft : undefined}>
+      <Flex self="start" align="start" justify="center" style={[ styles.button, absolute ? styles.absolute : undefined ]}>
         <IconButton
           name="backIcon"
           type="MissionHub"
           onPress={() => {
+            console.log(customNavigate);
             customNavigate ? customNavigate() : dispatch(navigateBack());
             Keyboard.dismiss(); // Always dismiss the keyboard when navigating back
           }}
--- src/containers/BackButton/styles.js
+++ src/containers/BackButton/styles.js
@@ -5,8 +5,10 @@ import { isiPhoneX } from '../../utils/common';
 const margin = 25;
 
 export default StyleSheet.create({
-  absoluteTopLeft: {
+  absolute: {
     position: 'absolute',
+  },
+  button: {
     top: isiPhoneX() ? margin + 25 : margin,
     left: 5,
   },
