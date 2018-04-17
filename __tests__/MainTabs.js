8ac8f35faac090d6153d594238cf333af534a9e4
Robert Eldredge <robert.eldredge@cru.org>
2018-04-17 10:52:36 -0400

    testSnapshot on full home screen, with tab bar

--- __tests__/MainTabs.js
+++ __tests__/MainTabs.js
@@ -1,16 +1,47 @@
 import 'react-native';
 import React from 'react';
+import { Provider } from 'react-redux';
 import theme from '../src/theme';
 
-import { renderShallow } from '../testUtils';
+import { createMockStore, renderShallow, testSnapshot } from '../testUtils';
 import { MainTabRoutes, navItem } from '../src/AppRoutes';
 
+const store = createMockStore({
+  steps: {
+    mine: [],
+    reminders: [],
+    userStepCount: {},
+    pagination: {
+      hasNextPage: true,
+      page: 1,
+    },
+  },
+  people: {
+    allByOrg: {
+      personal: { id: 'personal', people: {} },
+    },
+  },
+  notifications: {
+    token: '',
+    hasAsked: false,
+    shouldAsk: true,
+    showReminder: true,
+    pushDeviceId: '',
+  },
+  swipe: {
+    stepsHome: true,
+    stepsContact: true,
+    stepsReminder: false, // Never show on the reminders anymore
+    journey: true,
+  },
+});
 
-it('renders tab bar correctly', () => {
-  const tabNav = renderShallow(<MainTabRoutes />).dive().dive().dive().dive();
-  const tabBar = tabNav.find('withOrientation').dive();
 
-  expect(tabBar).toMatchSnapshot();
+it('renders home screen with tab bar correctly', () => {
+  testSnapshot(
+    <Provider store={store}>
+      <MainTabRoutes />
+    </Provider>);
 });
 
 it('renders navItem correctly', () => {
