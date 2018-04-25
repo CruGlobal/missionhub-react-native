34a2e0c8b951c3386d55dc2c2f6741272ab3df27
Scotty Waggoner <ozzieorca@gmail.com>
2018-04-25 13:24:16 -0700

    Update lock file and fix indents

--- android/app/src/main/java/com/missionhub/MainApplication.java
+++ android/app/src/main/java/com/missionhub/MainApplication.java
@@ -41,10 +41,10 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
-        @Override
-        protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
-        }
+    @Override
+      protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
     
     @Override
     public boolean getUseDeveloperSupport() {
