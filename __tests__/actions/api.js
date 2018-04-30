3c0466b3481b10cdea71c8759d6879c7d9050533
Robert Eldredge <robert.eldredge@cru.org>
2018-04-30 12:03:09 -0400

    Write test for invalid_grant error

--- __tests__/actions/api.js
+++ __tests__/actions/api.js
@@ -2,41 +2,44 @@ import callApi, { REQUESTS } from '../../src/actions/api';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import API_CALLS from '../../src/api';
-import { EXPIRED_ACCESS_TOKEN, UPDATE_TOKEN } from '../../src/constants';
+import { EXPIRED_ACCESS_TOKEN, INVALID_GRANT, UPDATE_TOKEN } from '../../src/constants';
 import { mockFnWithParams } from '../../testUtils';
 import * as auth from '../../src/actions/auth';
 import * as facebook from '../../src/actions/facebook';
 
 const token = 'alsnjfjwqfpuqfeownposfnjnsaobjfaslkklnsfd';
+const refreshToken = 'refresh';
 const mockStore = configureStore([ thunk ]);
 let store;
 const mockAuthState = {
   token,
 };
 
-const query = { access_token: token };
-const request = REQUESTS.GET_ME;
+const accessTokenQuery = { access_token: token };
+const refreshTokenData = `grant_type=refresh_token&refresh_token=${refreshToken}`;
 
-const error = {
-  errors: [ { detail: EXPIRED_ACCESS_TOKEN } ],
-};
+const getMeRequest = REQUESTS.GET_ME;
+const refreshRequest = REQUESTS.KEY_REFRESH_TOKEN;
+
+const expiredTokenError = { errors: [ { detail: EXPIRED_ACCESS_TOKEN } ] };
+const invalidGrantError = { error: INVALID_GRANT };
 
 global.APILOG = jest.fn();
 
 beforeEach(() => expect.assertions(1));
 
-async function test(state, obj, method, apiResult, ) {
+async function test(state, obj, request, error, method, methodParams, apiResult, query, data) {
   store = mockStore({ auth: { ...mockAuthState, ...state } });
-  mockFnWithParams(API_CALLS, request.name, Promise.reject({ apiError: error }), query, {});
-  mockFnWithParams(obj, method, apiResult);
+  mockFnWithParams(API_CALLS, request.name, Promise.reject({ apiError: error }), query, data);
+  mockFnWithParams(obj, method, apiResult, ...methodParams);
 
   try {
-    await store.dispatch(callApi(request, {}, {}));
+    await store.dispatch(callApi(request, query, data));
   } catch (e) {
     expect(store.getActions()).toEqual([
       {
-        data: {},
-        query: query,
+        data,
+        query,
         type: request.FETCH,
       },
       apiResult,
@@ -45,34 +48,38 @@ async function test(state, obj, method, apiResult, ) {
 }
 
 it('should refresh key access token if user is logged in with TheKey', () => {
-  return test({ refreshToken: 'refresh' }, auth, 'refreshAccessToken', { type: 'refreshed token' });
+  return test({ refreshToken: 'refresh' }, auth, getMeRequest, expiredTokenError, 'refreshAccessToken', [], { type: 'refreshed token' }, accessTokenQuery, {});
 });
 
 it('should refresh anonymous login if user is Try It Now', async() => {
-  return test({ isFirstTime: true }, auth, 'refreshAnonymousLogin', { type: 'refreshed anonymous token' });
+  return test({ isFirstTime: true }, auth, getMeRequest, expiredTokenError, 'refreshAnonymousLogin', [], { type: 'refreshed anonymous token' }, accessTokenQuery, {});
 });
 
 it('should refresh facebook login if user is not logged in with TheKey or Try It Now', async() => {
-  return test({}, facebook, 'refreshMissionHubFacebookAccess', { type: 'refreshed fb login' });
+  return test({}, facebook, getMeRequest, expiredTokenError, 'refreshMissionHubFacebookAccess', [], { type: 'refreshed fb login' }, accessTokenQuery, {});
+});
+
+it('should logout if KEY_REFRESH_TOKEN fails with invalid_grant', async() => {
+  return test({ refreshToken: 'refresh' }, auth, refreshRequest, invalidGrantError, 'logout', [ true ], { type: 'logged out' }, {}, refreshTokenData);
 });
 
 it('should update token if present in response', async() => {
   store = mockStore({ auth: { ...mockAuthState } });
   const newToken = 'pfiqwfioqwioefiqowfejiqwfipoioqwefpiowqniopnifiooiwfemiopqwoimefimwqefponioqwfenoiwefinonoiwqefnoip';
-  mockFnWithParams(API_CALLS, request.name, Promise.resolve({ sessionHeader: newToken }), query, {});
+  mockFnWithParams(API_CALLS, getMeRequest.name, Promise.resolve({ sessionHeader: newToken }), accessTokenQuery, {});
 
-  await store.dispatch(callApi(request, {}, {}));
+  await store.dispatch(callApi(getMeRequest, {}, {}));
 
   expect(store.getActions()).toEqual([
     {
       data: {},
-      query: query,
-      type: request.FETCH,
+      query: accessTokenQuery,
+      type: getMeRequest.FETCH,
     },
     {
       data: {},
-      query: query,
-      type: request.SUCCESS,
+      query: accessTokenQuery,
+      type: getMeRequest.SUCCESS,
       results: { response: undefined },
     },
     {
