122f0aabde9a3538420ffce7f32d05ad9f962dc3
Robert Eldredge <robert.eldredge@cru.org>
2018-04-30 17:30:45 -0400

    remove extra flex

--- src/containers/PathwayStageScreen/index.js
+++ src/containers/PathwayStageScreen/index.js
@@ -120,28 +120,24 @@ class PathwayStageScreen extends Component {
     return (
       <Flex align="center" justify="center" value={1} style={styles.container}>
         {this.props.enableBackButton ? <BackButton absolute={true} /> : null}
-        <Flex align="center" justify="center">
-          <Text style={styles.title}>
-            {this.props.questionText}
-          </Text>
-          {
-            this.props.stages ? (
-              <Carousel
-                firstItem={this.props.firstItem || fallbackIndex}
-                data={this.props.stages}
-                inactiveSlideOpacity={1}
-                inactiveSlideScale={1}
-                renderItem={this.renderStage}
-                sliderWidth={sliderWidth + 75}
-                itemWidth={stageWidth + stageMargin * 2}
-                onScroll={this.handleScroll}
-                scrollEventThrottle={5}
-                onSnapToItem={this.handleSnapToItem}
-                containerCustomStyle={{ height: 400, flex: 0, flexGrow: 0 }}
-              />
-            ) : null
-          }
-        </Flex>
+        <Text style={styles.title}>{this.props.questionText}</Text>
+        {
+          this.props.stages ? (
+            <Carousel
+              firstItem={this.props.firstItem || fallbackIndex}
+              data={this.props.stages}
+              inactiveSlideOpacity={1}
+              inactiveSlideScale={1}
+              renderItem={this.renderStage}
+              sliderWidth={sliderWidth + 75}
+              itemWidth={stageWidth + stageMargin * 2}
+              onScroll={this.handleScroll}
+              scrollEventThrottle={5}
+              onSnapToItem={this.handleSnapToItem}
+              containerCustomStyle={{ height: 400, flex: 0, flexGrow: 0 }}
+            />
+          ) : null
+        }
         <Image
           resizeMode="contain"
           source={LANDSCAPE}
