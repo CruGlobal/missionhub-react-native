0cca5a763b690066fc6d57dc37070136850d10a0
Robert Eldredge <robert.eldredge@cru.org>
2018-05-16 13:48:48 -0400

    refactor mapStateToProps

--- src/containers/MainTabs.js
+++ src/containers/MainTabs.js
@@ -6,14 +6,15 @@ import { MainTabBar, MainTabBarGroups } from '../AppRoutes';
 
 
 class MainTabs extends Component {
+
   render() {
     return (this.props.groups ? <MainTabBarGroups /> : <MainTabBar />);
   }
 }
 
-const mapStateToProps = ({ auth }) => {
-  return { groups: auth.groupsFeature };
-};
+const mapStateToProps = ({ auth }) => ({
+  groups: auth.groupsFeature,
+});
 
 export default connect(mapStateToProps)(MainTabs);
 export const MAIN_TABS = 'nav/MAIN_TABS';
