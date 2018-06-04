d8d3f8f998b2fb66e48e658d3b043826defd6336
Robert Eldredge <robert.eldredge@cru.org>
2018-06-04 13:57:27 -0400

    update test

--- src/containers/Groups/__tests__/GroupsListScreen.js
+++ src/containers/Groups/__tests__/GroupsListScreen.js
@@ -11,9 +11,28 @@ jest.mock('../../../actions/navigation', () => ({
   navigatePush: jest.fn(() => ({ type: 'test' })),
 }));
 
-const store = createMockStore({});
+const store = createMockStore({
+  organizations: {
+    all: [
+      {
+        id: '123',
+        name: 'Cru at Boston University',
+        contacts: 768,
+        unassigned: 13,
+        uncontacted: 43,
+      },
+      {
+        id: '456',
+        name: 'Cru at Boston University Northeast Branch',
+        contacts: 768,
+        unassigned: 0,
+        uncontacted: 0,
+      },
+    ],
+  },
+});
 
-describe('Contacts', () => {
+describe('GroupsListScreen', () => {
   let component;
   beforeEach(() => {
     component = renderShallow(<GroupsListScreen />, store);
