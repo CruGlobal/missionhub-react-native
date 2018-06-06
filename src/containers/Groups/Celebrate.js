0024f17fb688e894234b924845acd5c38507e44e
Robert Eldredge <robert.eldredge@cru.org>
2018-06-06 15:04:26 -0400

    create inverted list

--- src/containers/Groups/Celebrate.js
+++ src/containers/Groups/Celebrate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 
@@ -11,14 +11,23 @@ import styles from './styles';
 
 @translate('groupsCelebrate')
 export class Celebrate extends Component {
-  renderSectionHeader(date) {
+  handleLoadMore = () => {
+    return true;
+  };
+
+  renderSection(item) {
     return (
       <Flex align="center" justify="center" style={styles.cardSectionHeader}>
         <DateComponent
-          date={date}
+          date={item.date}
           style={styles.cardSectionText}
           format="dddd, MMMM D"
         />
+        {item.events
+          ? item.events.map(event => (
+              <CelebrateItem key={event.id} event={event} />
+            ))
+          : null}
       </Flex>
     );
   }
@@ -26,19 +35,14 @@ export class Celebrate extends Component {
   render() {
     return (
       <Flex value={1}>
-        <ScrollView style={styles.cardList}>
-          {this.props.items.map(date => (
-            <Flex key={date.id}>
-              {this.renderSectionHeader(date.date)}
-              {date.events
-                ? date.events.map(event => (
-                    <CelebrateItem key={event.id} event={event} />
-                  ))
-                : null}
-            </Flex>
-          ))}
-          <LoadMore onPress={this.handleLoadMore} />
-        </ScrollView>
+        <FlatList
+          data={this.props.items}
+          keyExtractor={i => i.id}
+          renderItem={({ item }) => this.renderSection(item)}
+          style={styles.cardList}
+          inverted={true}
+          ListFooterComponent={<LoadMore onPress={this.handleLoadMore} />}
+        />
         <Flex justify="end">
           <Button
             type="secondary"
@@ -55,47 +59,47 @@ const mapStateToProps = () => ({
   items: [
     {
       date: '2018-04-11 19:06:00 UTC',
-      id: 1,
+      id: '1',
       events: [
         {
-          id: 1,
+          id: '1',
           full_name: 'Matt Watts',
           changed_attribute_value: '2018-04-11 19:06:00 UTC',
           title: 'Matt had a Discipleship Conversation',
-          likes_count: 3,
+          likes_count: '3',
         },
       ],
     },
     {
       date: '2018-04-10 07:06:00 UTC',
-      id: 2,
+      id: '2',
       events: [
         {
-          id: 2,
+          id: '2',
           full_name: 'Emma Frost',
           changed_attribute_value: '2018-04-10 17:32:00 UTC',
           title: 'Matt added a Curious person',
-          likes_count: 5,
+          likes_count: '5',
         },
         {
-          id: 3,
+          id: '3',
           full_name: 'Emma Frost',
           changed_attribute_value: '2018-04-10 07:06:00 UTC',
           title: 'Emma had a Spiritual Conversation',
-          likes_count: 3,
+          likes_count: '3',
         },
       ],
     },
     {
       date: '2018-04-09 15:23:00 UTC',
-      id: 3,
+      id: '3',
       events: [
         {
-          id: 4,
+          id: '4',
           full_name: 'Leah Brooks',
           changed_attribute_value: '2018-04-09 15:23:00 UTC',
           title: 'Leah completed a Step of Faith with a Curious person',
-          likes_count: 0,
+          likes_count: '0',
         },
       ],
     },
