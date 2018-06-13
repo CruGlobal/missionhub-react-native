682d925835d648a386d5c20be95156313e97e4da
Robert Eldredge <robert.eldredge@cru.org>
2018-06-13 09:41:07 -0400

    More tests

--- __tests__/reducers/organizations.js
+++ __tests__/reducers/organizations.js
@@ -3,6 +3,7 @@ import {
   GET_ORGANIZATION_CONTACTS,
   GET_ORGANIZATION_SURVEYS,
 } from '../../src/constants';
+import { REQUESTS } from '../../src/actions/api';
 
 const orgId = '123';
 const initialState = {
@@ -92,4 +93,73 @@ it('loads surveys for org with paging', () => {
   );
 
   expect(state.surveysPagination).toEqual({ hasNextPage: false, page: 2 });
+});
+
+it('loads celebrate items with pagination', () => {
+  const orgId = '1';
+  const oldItems = [
+    { id: '1', title: 'Title 1' },
+    { id: '2', title: 'Title 2' },
+    { id: '3', title: 'Title 3' },
+    { id: '4', title: 'Title 4' },
+    { id: '5', title: 'Title 5' },
+    { id: '6', title: 'Title 6' },
+    { id: '7', title: 'Title 7' },
+    { id: '8', title: 'Title 8' },
+    { id: '9', title: 'Title 9' },
+    { id: '10', title: 'Title 10' },
+    { id: '11', title: 'Title 11' },
+    { id: '12', title: 'Title 12' },
+    { id: '13', title: 'Title 13' },
+    { id: '14', title: 'Title 14' },
+    { id: '15', title: 'Title 15' },
+    { id: '16', title: 'Title 16' },
+    { id: '17', title: 'Title 17' },
+    { id: '18', title: 'Title 18' },
+    { id: '19', title: 'Title 19' },
+    { id: '20', title: 'Title 20' },
+    { id: '21', title: 'Title 21' },
+    { id: '22', title: 'Title 22' },
+    { id: '23', title: 'Title 23' },
+    { id: '24', title: 'Title 24' },
+  ];
+  const newItems = [
+    { id: '25', title: 'Title 25' },
+    { id: '26', title: 'Title 26' },
+    { id: '27', title: 'Title 27' },
+  ];
+
+  const state = organizations(
+    {
+      all: [
+        {
+          id: orgId,
+          surveys: oldItems,
+        },
+      ],
+      celebratePagination: {
+        hasNextPage: true,
+        page: 1,
+      },
+    },
+    {
+      type: REQUESTS.GET_GROUP_CELEBRATE_FEED.SUCCESS,
+      orgId: orgId,
+      query: {
+        page: {
+          limit: 25,
+          offset: 25,
+        },
+        orgId,
+      },
+      meta: {
+        total: 28,
+      },
+      results: {
+        response: newItems,
+      },
+    },
+  );
+
+  expect(state.celebratePagination).toEqual({ hasNextPage: false, page: 2 });
 });
