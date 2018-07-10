d11eb91cb0cc399bfc427ec0e3868def532e96cf
ryancarlson <ryan.t.carlson@cru.org>
2018-07-10 16:40:26 -0400

    extract a function for building celebration query
    it optionally adds a person_id filter

--- src/actions/celebration.js
+++ src/actions/celebration.js
@@ -2,20 +2,33 @@ import { DEFAULT_PAGE_LIMIT } from '../constants';
 
 import callApi, { REQUESTS } from './api';
 
-export function getGroupCelebrateFeed(orgId) {
+export function getGroupCelebrateFeed(orgId, personId = null) {
   return (dispatch, getState) => {
     const { page, hasNextPage } = getState().organizations.celebratePagination;
     if (!hasNextPage) {
       return Promise.reject('NoMoreData');
     }
-    const query = {
-      page: {
-        limit: DEFAULT_PAGE_LIMIT,
-        offset: DEFAULT_PAGE_LIMIT * page,
-      },
-      orgId,
-    };
+    const query = buildQuery(orgId, personId, page);
 
     dispatch(callApi(REQUESTS.GET_GROUP_CELEBRATE_FEED, query));
   };
+}
+
+function buildQuery(orgId, personId, page) {
+  let query = {
+    page: {
+      limit: DEFAULT_PAGE_LIMIT,
+      offset: DEFAULT_PAGE_LIMIT * page,
+    },
+    orgId,
+  };
+
+  if (personId) {
+    query = {
+      ...query,
+      person_id: personId,
+    };
+  }
+
+  return query;
 }
