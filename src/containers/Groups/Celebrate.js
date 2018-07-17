502f3e7ec27730bbf0ac7f913dcc2e265361c9af
ryancarlson <ryan.t.carlson@cru.org>
2018-07-17 12:49:15 -0400

    Boom! creating a reusable component just works!

--- src/containers/Groups/Celebrate.js
+++ src/containers/Groups/Celebrate.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 
-import {
-  Flex,
-  DateComponent,
-  PlatformKeyboardAvoidingView,
-} from '../../components/common';
-import CelebrateItem from '../../components/CelebrateItem';
-import LoadMore from '../../components/LoadMore';
-import CommentBox from '../../components/CommentBox';
+import CelebrateFeed from '../../components/CelebrateFeed';
 import { getGroupCelebrateFeed } from '../../actions/celebration';
 import { organizationSelector } from '../../selectors/organizations';
 import { celebrationSelector } from '../../selectors/celebration';
-
-import styles from './styles';
 
 @translate('groupsCelebrate')
 export class Celebrate extends Component {
@@ -28,46 +18,13 @@ export class Celebrate extends Component {
     dispatch(getGroupCelebrateFeed(organization.id));
   };
 
-  renderSection = item => {
-    return (
-      <Flex align="center" justify="center">
-        <DateComponent
-          date={item.date}
-          style={styles.cardSectionHeader}
-          format="relative"
-        />
-        {item.events
-          ? item.events.map(event => (
-              <CelebrateItem key={event.id} event={event} />
-            ))
-          : null}
-      </Flex>
-    );
-  };
-
   submit = data => {
     return data;
   };
 
   render() {
-    const { t } = this.props;
-    return (
-      <PlatformKeyboardAvoidingView>
-        <FlatList
-          data={this.props.celebrateItems}
-          keyExtractor={i => i.id}
-          renderItem={({ item }) => this.renderSection(item)}
-          style={styles.cardList}
-          inverted={true}
-          ListFooterComponent={<LoadMore onPress={this.loadItems} />}
-        />
-        <CommentBox
-          placeholder={t('placeholder')}
-          hideActions={true}
-          onSubmit={this.submit}
-        />
-      </PlatformKeyboardAvoidingView>
-    );
+    const { celebrateItems } = this.props;
+    return <CelebrateFeed items={celebrateItems} />;
   }
 }
 
