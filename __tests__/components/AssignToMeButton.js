8ed4eda34ea3af1e4d82b5cc42886cacd248dba0
dsgoers <daniel.goers@cru.org>
2018-07-19 17:42:38 -0400

    Fix another test

--- __tests__/components/AssignToMeButton.js
+++ __tests__/components/AssignToMeButton.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
 
 import { testSnapshotShallow } from '../../testUtils';
 import AssignToMeButton from '../../src/components/AssignToMeButton';
 
+const myId = '25';
+const store = configureStore([thunk])({
+  auth: { person: { id: myId } },
+});
+
+const props = {
+  personId: '100',
+  orgId: '800',
+};
+
 it('renders correctly', () => {
-  testSnapshotShallow(<AssignToMeButton onPress={jest.fn()} />);
+  testSnapshotShallow(<AssignToMeButton {...props} />, store);
 });
