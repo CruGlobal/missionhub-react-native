8d1232b6a5e741900fcc1d5005aaa2554a21bef9
ryancarlson <ryan.t.carlson@cru.org>
2018-07-19 16:52:29 -0400

    Add test to test rendering populated and empty feed for member

--- __tests__/containers/MemberCelebrate.js
+++ __tests__/containers/MemberCelebrate.js
@@ -5,29 +5,64 @@ import thunk from 'redux-thunk';
 import MemberCelebrate from '../../src/containers/MemberCelebrate';
 import { testSnapshotShallow } from '../../testUtils';
 
-const mockState = {
+const mockEmptyState = {
   organizations: {
     all: [
       {
         id: '123',
         celebrateItems: [],
+        celebratePagination: {
+          hasNextPage: true,
+          page: 0,
+        },
       },
     ],
-    celebratePagination: {
-      hasNextPage: true,
-      page: 0,
-    },
+  },
+};
+
+const mockState = {
+  organizations: {
+    all: [
+      {
+        id: '456',
+        celebrateItems: [
+          {
+            id: '10',
+            celebrateable_type: 'interaction',
+            changed_attribute_value: '2018-06-11 00:00:00 UTC',
+            subject_person: { id: '1' },
+          },
+        ],
+        celebratePagination: {
+          hasNextPage: true,
+          page: 0,
+        },
+      },
+    ],
   },
 };
 
 const mockStore = configureStore([thunk]);
 
-let store = mockStore(mockState);
+const emptyStore = mockStore(mockEmptyState);
+const store = mockStore(mockState);
 
 describe('MemberCelebrate', () => {
-  it('renders correctly', () => {
+  it('renders empty feed correctly', () => {
     const person = { id: '1' };
     const organization = { id: '123' };
+    testSnapshotShallow(
+      <MemberCelebrate
+        store={emptyStore}
+        person={person}
+        organization={organization}
+      />,
+    );
+  });
+
+  it('renders correctly', () => {
+    const person = { id: '1' };
+    const organization = { id: '456' };
     testSnapshotShallow(
       <MemberCelebrate
         store={store}
