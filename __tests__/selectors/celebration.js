b229230640a1dc4cfd2a284c69d53c8383fce0a4
ryancarlson <ryan.t.carlson@cru.org>
2018-07-20 15:53:28 -0400

    Add a test to make sure items are filtered out

--- __tests__/selectors/celebration.js
+++ __tests__/selectors/celebration.js
@@ -45,6 +45,33 @@ const celebrateItems = [
   },
 ];
 
+const invalidItems = [
+  {
+    id: '11',
+    celebrateable_type: 'interaction',
+    adjective_attribute_value: 42,
+    changed_attribute_value: '2018-01-01 00:00:00 UTC',
+  },
+  {
+    id: '12',
+    celebrateable_type: 'interaction',
+    adjective_attribute_value: 1,
+    changed_attribute_value: '2017-01-01 00:00:00 UTC',
+  },
+  {
+    id: '13',
+    celebrateable_type: 'roge',
+    adjective_attribute_value: 11,
+    changed_attribute_value: '2018-01-02 00:07:00 UTC',
+  },
+];
+
 it('sorts items into sections by date', () => {
   expect(celebrationSelector({ celebrateItems })).toMatchSnapshot();
+});
+
+it('filters out celebrate items it cannot render', () => {
+  const combinedItems = celebrateItems.concat(invalidItems);
+  const selectedCelebrationItems = celebrationSelector({ combinedItems });
+  expect(selectedCelebrationItems).toMatchSnapshot();
 });
