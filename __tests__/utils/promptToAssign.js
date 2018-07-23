606e06139073a0d4fc981aa21dc683dddab5da96
dsgoers <daniel.goers@cru.org>
2018-07-23 15:43:15 -0400

    Add a couple more tests for promptToAssign()

--- __tests__/utils/promptToAssign.js
+++ __tests__/utils/promptToAssign.js
@@ -5,11 +5,11 @@ import i18n from '../../src/i18n';
 
 jest.mock('../../src/i18n');
 
-Alert.alert = jest.fn().mockReturnValue(true);
-
 beforeEach(() => (i18n.t = jest.fn(string => string)));
 
 it('raises an alert', () => {
+  Alert.alert = jest.fn().mockReturnValue(true);
+
   promptToAssign();
 
   expect(Alert.alert).toHaveBeenCalledWith(
@@ -29,4 +29,28 @@ it('raises an alert', () => {
     ],
     { onDismiss: expect.anything() },
   );
+});
+
+it('returns false when cancel is clicked', async () => {
+  Alert.alert = jest
+    .fn()
+    .mockImplementation((_, __, buttons) => buttons[0].onPress());
+
+  expect(await promptToAssign()).toBe(false);
+});
+
+it('returns true when continue is clicked', async () => {
+  Alert.alert = jest
+    .fn()
+    .mockImplementation((_, __, buttons) => buttons[1].onPress());
+
+  expect(await promptToAssign()).toBe(true);
+});
+
+it('returns true when modal is dismissed', async () => {
+  Alert.alert = jest
+    .fn()
+    .mockImplementation((_, __, ___, options) => options.onDismiss());
+
+  expect(await promptToAssign()).toBe(false);
 });
