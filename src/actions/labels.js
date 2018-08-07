fb94589d0766511e5567981768624bcef52e0d36
Robert Eldredge <robert.eldredge@cru.org>
2018-08-07 13:51:15 -0400

    endpoint for organization labels

--- src/actions/labels.js
+++ src/actions/labels.js
@@ -9,3 +9,14 @@ export function getMyLabels() {
     return dispatch(callApi(REQUESTS.GET_MY_LABELS, query));
   };
 }
+
+export function getOrgLabels(orgId) {
+  return dispatch => {
+    const query = {
+      orgId,
+      limit: 100,
+      include: 'labels',
+    };
+    return dispatch(callApi(REQUESTS.GET_ORGANIZATION_LABELS, query));
+  };
+}
