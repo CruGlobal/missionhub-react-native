9d2714787ffbe487da837a2c8477e37ac47b79fa
Bryan Eaton <bryaneaton13@gmail.com>
2018-08-16 10:05:26 -0400

    Fix how we determine if a conact is in a personal ministry for the journey page.

--- src/containers/ContactJourney/index.js
+++ src/containers/ContactJourney/index.js
@@ -24,7 +24,7 @@ class ContactJourney extends Component {
   constructor(props) {
     super(props);
 
-    const isPersonal = !props.isCasey && !props.organization;
+    const isPersonal = props.isCasey || !props.organization;
 
     this.state = {
       editingInteraction: null,
