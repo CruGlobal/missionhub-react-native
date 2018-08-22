16dba9a122f717c318b9fdb70dd80ebc91156a04
Robert Eldredge <robert.eldredge@cru.org>
2018-08-22 15:44:30 -0400

    move backHandler functionality back to AppNavigator.js

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
 import { reduxifyNavigator } from 'react-navigation-redux-helpers';
+import React from 'react';
+import { BackHandler } from 'react-native';
 
 import { MainRoutes } from './AppRoutes';
+import { navigateBack } from './actions/navigation';
 
 const app = reduxifyNavigator(MainRoutes, 'root');
 
@@ -9,4 +12,30 @@ const mapStateToProps = ({ nav }) => ({
   state: nav,
 });
 
-export default connect(mapStateToProps)(app);
+const appWithNavState = connect(mapStateToProps)(app);
+
+export default backHandlerWrapper(appWithNavState);
+
+function backHandlerWrapper(WrappedComponent) {
+  class wrapper extends React.Component {
+    componentDidMount() {
+      BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+    }
+    componentWillUnmount() {
+      BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+    }
+    onBackPress = () => {
+      const { dispatch, state } = this.props;
+      if (state.index === 0) {
+        return false;
+      }
+      dispatch(navigateBack());
+      return true;
+    };
+    render() {
+      return <WrappedComponent />;
+    }
+  }
+
+  return connect(mapStateToProps)(wrapper);
+}
