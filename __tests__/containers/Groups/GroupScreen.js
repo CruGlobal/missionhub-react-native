a8d14a2f4711ab802cd27ea88e019fad748d1605
Robert Eldredge <robert.eldredge@cru.org>
2018-10-05 11:14:58 -0400

    update snapshots

--- __tests__/containers/Groups/GroupScreen.js
+++ __tests__/containers/Groups/GroupScreen.js
@@ -15,19 +15,23 @@ jest.mock('../../../src/actions/navigation', () => ({
   navigatePush: jest.fn(),
 }));
 
-const organization = { id: '5', name: 'Test  Org' };
+const organization = { id: '5', name: 'Test  Org', user_created: false };
 
 describe('GroupScreen', () => {
-  const header = (
+  const createHeader = org => (
     <GroupScreen
       navigation={createMockNavState({
-        organization: { id: '5', name: 'Test  Org' },
+        organization: org,
       })}
     />
   );
 
   it('should render header correctly', () => {
-    testSnapshotShallow(header);
+    testSnapshotShallow(createHeader(organization));
+  });
+
+  it('should render header correctly for user_created org', () => {
+    testSnapshotShallow(createHeader({ ...organization, user_created: true }));
   });
 
   it('should handle add contact button correctly', () => {
