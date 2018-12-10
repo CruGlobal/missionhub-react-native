efc60587931e42bee919e95c1b5e31935443ea58
Robert Eldredge <robert.eldredge@cru.org>
2018-12-10 16:57:44 -0500

    finish SetupScreen and KeyLoginScreen in flow

--- src/routes/deepLink/deepLinkJoinCommunityUnauthenticated.js
+++ src/routes/deepLink/deepLinkJoinCommunityUnauthenticated.js
@@ -1,7 +1,8 @@
 import { createStackNavigator } from 'react-navigation';
 
-import { navigatePush } from '../../actions/navigation';
 import { buildTrackedScreen, wrapNextAction } from '../helpers';
+import { buildTrackingObj, isAndroid } from '../../utils/common';
+import { navigatePush, navigateReset } from '../../actions/navigation';
 import { joinCommunity } from '../../actions/organizations';
 import { setScrollGroups } from '../../actions/swipe';
 import { firstTime, loadHome } from '../../actions/auth';
@@ -17,10 +18,15 @@ import SetupScreen, { SETUP_SCREEN } from '../../containers/SetupScreen';
 import KeyLoginScreen, {
   KEY_LOGIN_SCREEN,
 } from '../../containers/KeyLoginScreen';
+import NotificationPrimerScreen, {
+  NOTIFICATION_PRIMER_SCREEN,
+} from '../../containers/NotificationPrimerScreen';
 import {
   GROUP_SCREEN,
   USER_CREATED_GROUP_SCREEN,
 } from '../../containers/Groups/GroupScreen';
+import { trackActionWithoutData } from '../../actions/analytics';
+import { ACTIONS } from '../../constants';
 
 export const DeepLinkJoinCommunityUnauthenticatedScreens = {
   [DEEP_LINK_CONFIRM_JOIN_GROUP_SCREEN]: buildTrackedScreen(
@@ -37,8 +43,18 @@ export const DeepLinkJoinCommunityUnauthenticatedScreens = {
       dispatch(navigatePush(signin ? KEY_LOGIN_SCREEN : SETUP_SCREEN));
     }),
   ),
-  [SETUP_SCREEN]: buildTrackedScreen(SetupScreen, () => {}),
-  [KEY_LOGIN_SCREEN]: buildTrackedScreen(KeyLoginScreen, () => {}),
+  [SETUP_SCREEN]: buildTrackedScreen(
+    SetupScreen,
+    () => (dispatch, getState) => {
+      dispatch(firstTime());
+      joinCommunityAndFinishOnboarding(dispatch, getState);
+    },
+  ),
+  [KEY_LOGIN_SCREEN]: buildTrackedScreen(
+    wrapNextAction(KeyLoginScreen, () => (dispatch, getState) => {
+      joinCommunityAndFinishOnboarding(dispatch, getState);
+    }),
+  ),
 };
 export const DeepLinkJoinCommunityUnauthenticatedNavigator = createStackNavigator(
   DeepLinkJoinCommunityUnauthenticatedScreens,
@@ -48,3 +64,35 @@ export const DeepLinkJoinCommunityUnauthenticatedNavigator = createStackNavigato
     },
   },
 );
+
+const joinCommunityAndFinishOnboarding = async (dispatch, getState) => {
+  dispatch(completeOnboarding());
+
+  const { community } = getState().profile;
+  await dispatch(joinCommunity(community.id, community.community_code));
+
+  const notificationScreensPromise = isAndroid
+    ? Promise.resolve()
+    : new Promise(resolve =>
+        dispatch(
+          navigatePush(NOTIFICATION_PRIMER_SCREEN, {
+            onComplete: resolve,
+            descriptionText: i18next.t(
+              'notificationPrimer:onboardingDescription',
+            ),
+          }),
+        ),
+      );
+
+  await Promise.all([dispatch(loadHome()), notificationScreensPromise]);
+
+  dispatch(
+    navigateReset(
+      community.user_created ? USER_CREATED_GROUP_SCREEN : GROUP_SCREEN,
+      {
+        organization: community,
+      },
+    ),
+  );
+  dispatch(trackActionWithoutData(ACTIONS.SELECT_JOINED_COMMUNITY));
+};
