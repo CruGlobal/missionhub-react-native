b458014d9c20f34f649079b625c638ecd7d3c4bc
Robert Eldredge <robert.eldredge@cru.org>
2019-01-09 17:04:43 -0500

    Shrink line spacing

--- src/components/GroupMemberItem/styles.js
+++ src/components/GroupMemberItem/styles.js
@@ -7,19 +7,22 @@ export default StyleSheet.create({
     paddingVertical: 10,
     paddingLeft: 16,
     paddingRight: 0,
-    height: 50,
-  },
-  detailsWrap: {
-    marginTop: 1,
+    height: 48,
   },
   name: {
     fontSize: 16,
+    lineHeight: 16,
     fontWeight: 'bold',
     color: theme.primaryColor,
+    includeFontPadding: false,
+    textAlignVertical: 'center',
   },
   detailText: {
     fontSize: 14,
+    lineHeight: 14,
     color: theme.grey1,
+    includeFontPadding: false,
+    textAlignVertical: 'center',
   },
   detailTextRed: {
     fontSize: 14,
