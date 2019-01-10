24b22a27b5ddeb97ba4de659f779fc3605174c75
Robert Eldredge <robert.eldredge@cru.org>
2019-01-10 13:39:46 -0500

    Revert "Shrink line spacing"
    This reverts commit b458014d9c20f34f649079b625c638ecd7d3c4bc.

--- src/components/GroupMemberItem/styles.js
+++ src/components/GroupMemberItem/styles.js
@@ -7,22 +7,19 @@ export default StyleSheet.create({
     paddingVertical: 10,
     paddingLeft: 16,
     paddingRight: 0,
-    height: 48,
+    height: 50,
+  },
+  detailsWrap: {
+    marginTop: 1,
   },
   name: {
     fontSize: 16,
-    lineHeight: 16,
     fontWeight: 'bold',
     color: theme.primaryColor,
-    includeFontPadding: false,
-    textAlignVertical: 'center',
   },
   detailText: {
     fontSize: 14,
-    lineHeight: 14,
     color: theme.grey1,
-    includeFontPadding: false,
-    textAlignVertical: 'center',
   },
   detailTextRed: {
     fontSize: 14,
