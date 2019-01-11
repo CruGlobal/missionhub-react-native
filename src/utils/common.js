21b9e271bef375c96e90d530bc116170bc816f52
Robert Eldredge <robert.eldredge@cru.org>
2019-01-11 17:28:12 -0500

    new icons and update journeyWarning to statusIcon

--- src/utils/common.js
+++ src/utils/common.js
@@ -187,9 +187,9 @@ export const getIconName = (type, interaction_type_id) => {
   } else if (type === 'answer_sheet') {
     return 'surveyIcon';
   } else if (type === 'contact_assignment') {
-    return 'journeyWarning';
+    return 'statusIcon';
   } else if (type === 'contact_unassignment') {
-    return 'journeyWarning';
+    return 'statusIcon';
   } else if (type === 'interaction') {
     const interaction = interactionsArr.find(i => i.id === interaction_type_id);
     if (interaction) {
