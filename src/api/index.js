f2ec3a1686821a2ba02ae2f1db6489566f350512
Scotty Waggoner <ozzieorca@gmail.com>
2019-01-17 15:27:22 -0800

    Convert API promises to async/await

--- src/api/index.js
+++ src/api/index.js
@@ -13,94 +13,90 @@ const VALID_METHODS = ['get', 'put', 'post', 'delete'];
 // Setup API call
 const API_CALLS = {};
 lodashForEach(apiRoutes, (routeData, key) => {
-  API_CALLS[key] = (q, d) =>
-    new Promise((resolve, reject) => {
-      const method = routeData.method || 'get';
+  API_CALLS[key] = async (q, d) => {
+    const method = routeData.method || 'get';
 
-      // Make sure the 'method' is one of the valid types
-      if (!VALID_METHODS.includes(method)) {
-        reject('InvalidMethod');
-        return;
+    // Make sure the 'method' is one of the valid types
+    if (!VALID_METHODS.includes(method)) {
+      throw 'InvalidMethod';
+    }
+    // Run a check to see if the data has all the correct info
+    if (routeData.beforeCall) {
+      const shouldContinueMsg = routeData.beforeCall(q, d);
+      if (shouldContinueMsg !== true) {
+        throw shouldContinueMsg;
       }
-      // Run a check to see if the data has all the correct info
-      if (routeData.beforeCall) {
-        const shouldContinueMsg = routeData.beforeCall(q, d);
-        if (shouldContinueMsg !== true) {
-          reject(shouldContinueMsg);
-          return;
+    }
+
+    // Merge the extra data with the access_token
+    const authHeader = {};
+    if (!routeData.anonymous) {
+      authHeader['Authorization'] = `Bearer ${q.access_token}`;
+    }
+    delete q.access_token;
+    const extra = merge({}, { headers: authHeader }, routeData.extra);
+
+    // Merge some default data from the routes with the data passed in
+    const data = dontMergeData(routeData) ? d : merge({}, routeData.data, d);
+    const query = merge({}, routeData.query, q);
+
+    // Get the endpoint either from the query, or the routeData
+    let endpoint = query.endpoint || routeData.endpoint;
+    //endpoint = endpoint + 'trouble';
+
+    // Only do this for endpoints that have query parameters
+    if (endpoint.includes('/:')) {
+      // Replace all `:orgId` with the query param
+      Object.keys(query).forEach(k => {
+        if (query[k] && endpoint.includes(`:${k}`)) {
+          endpoint = endpoint.replace(`:${k}`, query[k]);
+          delete query[k];
         }
-      }
+      });
+    }
 
-      // Merge the extra data with the access_token
-      const authHeader = {};
-      if (!routeData.anonymous) {
-        authHeader['Authorization'] = `Bearer ${q.access_token}`;
-      }
-      delete q.access_token;
-      const extra = merge({}, { headers: authHeader }, routeData.extra);
-
-      // Merge some default data from the routes with the data passed in
-      const data = dontMergeData(routeData) ? d : merge({}, routeData.data, d);
-      const query = merge({}, routeData.query, q);
-
-      // Get the endpoint either from the query, or the routeData
-      let endpoint = query.endpoint || routeData.endpoint;
-      //endpoint = endpoint + 'trouble';
-
-      // Only do this for endpoints that have query parameters
-      if (endpoint.includes('/:')) {
-        // Replace all `:orgId` with the query param
-        Object.keys(query).forEach(k => {
-          if (query[k] && endpoint.includes(`:${k}`)) {
-            endpoint = endpoint.replace(`:${k}`, query[k]);
-            delete query[k];
-          }
-        });
-      }
-
-      // Call the request
-      APILOG(`${key} FETCH`, method, endpoint, query, data);
-      request(
+    // Call the request
+    APILOG(`${key} FETCH`, method, endpoint, query, data);
+    try {
+      const { jsonResponse, sessionHeader } = await request(
         method,
         endpoint,
         query,
         method === 'get' ? undefined : data,
         extra,
-      )
-        .then(({ jsonResponse, sessionHeader }) => {
-          APILOG(`${key} SUCCESS`, jsonResponse);
-          if (!jsonResponse) {
-            resolve({ sessionHeader });
-            return;
-          }
+      );
+      APILOG(`${key} SUCCESS`, jsonResponse);
+      if (!jsonResponse) {
+        return { sessionHeader };
+      }
 
-          if (
-            exists(routeData.useJsonDataApiStore) &&
-            !routeData.useJsonDataApiStore
-          ) {
-            resolve({ results: jsonResponse, meta: jsonResponse.meta });
-          } else {
-            const jsonApiStore = new JsonApiDataStore();
-            const response = jsonApiStore.sync(jsonResponse);
-            resolve({
-              meta: jsonResponse.meta,
-              results: jsonApiStore,
-              response,
-              sessionHeader,
-            });
-          }
-        })
-        .catch(apiError => {
-          LOG(
-            'request error or error in logic that handles the request',
-            key,
-            apiError,
-          );
-          APILOG(`${key} FAIL`, apiError);
+      if (
+        exists(routeData.useJsonDataApiStore) &&
+        !routeData.useJsonDataApiStore
+      ) {
+        return { results: jsonResponse, meta: jsonResponse.meta };
+      } else {
+        const jsonApiStore = new JsonApiDataStore();
+        const response = jsonApiStore.sync(jsonResponse);
 
-          return reject({ key, endpoint, method, query, apiError });
-        });
-    });
+        return {
+          meta: jsonResponse.meta,
+          results: jsonApiStore,
+          response,
+          sessionHeader,
+        };
+      }
+    } catch (apiError) {
+      LOG(
+        'request error or error in logic that handles the request',
+        key,
+        apiError,
+      );
+      APILOG(`${key} FAIL`, apiError);
+
+      throw { key, endpoint, method, query, apiError };
+    }
+  };
 });
 
 const dontMergeData = routeData =>
