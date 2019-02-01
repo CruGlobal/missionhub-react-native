87b3a560caf3fd276cf3d7a84f77f7d4e025c1ea
dsgoers <daniel.goers@cru.org>
2019-02-01 09:43:25 -0500

    Remove some unused constants

--- src/reducers/tabs.js
+++ src/reducers/tabs.js
@@ -1,7 +1,5 @@
 import {
   MAIN_TAB_CHANGED,
-  CONTACT_TAB_CHANGED,
-  LOGIN_TAB_CHANGED,
   PERSON_VIEWED_STAGE_CHANGED,
   SELF_VIEWED_STAGE_CHANGED,
 } from '../constants';
@@ -21,20 +19,10 @@ const initialTabsState = {
 
 export default function tabsReducer(state = initialTabsState, action) {
   switch (action.type) {
-    case LOGIN_TAB_CHANGED:
-      return {
-        ...state,
-        activeLoginTab: action.newActiveTab,
-      };
     case MAIN_TAB_CHANGED:
       return {
         ...state,
         activeMainTab: action.newActiveTab,
-      };
-    case CONTACT_TAB_CHANGED:
-      return {
-        ...state,
-        activeContactTab: action.newActiveTab,
       };
     case PERSON_VIEWED_STAGE_CHANGED:
       return {
