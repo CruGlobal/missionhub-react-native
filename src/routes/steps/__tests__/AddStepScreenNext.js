2b93230ff214cfd0085e59ad46c411db7e3191d5
Robert Eldredge <robert.eldredge@cru.org>
2019-02-04 11:25:25 -0500

    Separate completeStepFlow tests into multiple files

--- src/routes/steps/__tests__/AddStepScreenNext.js
+++ src/routes/steps/__tests__/AddStepScreenNext.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import * as reactNavigation from 'react-navigation';
 
 import { RESET_STEP_COUNT, STEP_NOTE } from '../../../constants';
 import { renderShallow } from '../../../../testUtils';
-import { buildTrackingObj } from '../../../utils/common';
 import { CompleteStepFlowScreens } from '../completeStepFlow';
-import { navigatePush, navigateBack } from '../../../actions/navigation';
-import { reloadJourney } from '../../../actions/journey';
+import { paramsforStageNavigation } from '../utils';
+import { navigatePush } from '../../../actions/navigation';
 import { ADD_STEP_SCREEN } from '../../../containers/AddStepScreen';
-import { CELEBRATION_SCREEN } from '../../../containers/CelebrationScreen';
 import { STAGE_SCREEN } from '../../../containers/StageScreen';
 import { PERSON_STAGE_SCREEN } from '../../../containers/PersonStageScreen';
-import { SELECT_MY_STEP_SCREEN } from '../../../containers/SelectMyStepScreen';
-import { PERSON_SELECT_STEP_SCREEN } from '../../../containers/PersonSelectStepScreen';
-import { paramsforStageNavigation } from '../utils';
+import { CELEBRATION_SCREEN } from '../../../containers/CelebrationScreen';
 
 jest.mock('../utils');
 jest.mock('../../../actions/navigation');
 jest.mock('../../../actions/steps');
-jest.mock('../../../actions/analytics');
-jest.mock('../../../actions/journey');
-jest.mock('../../../selectors/people');
 
 const myId = '111';
 const otherId = '222';
@@ -33,15 +25,12 @@ const orgId = '123';
 const contactAssignmentId = '22';
 const questionText = 'Text';
 
-const stage = { id: 1 };
 const reverseContactAssignment = {
   id: contactAssignmentId,
 };
 
 const store = configureStore([thunk])({
   auth: { person: { id: myId, user: { pathway_stage_id: 0 } } },
-  personProfile: { id: '1', personFirstName: otherName },
-  profile: { id: '2', firstName: myName },
 });
 
 const buildAndCallNext = async (screen, navParams, nextProps) => {
@@ -290,192 +279,5 @@ describe('AddStepScreen, not isMe, has completed 3 steps', () => {
       name: otherName,
     });
     expect(store.getActions()).toEqual([navigatePushResponse]);
-  });
-});
-
-describe('StageScreen, isAlreadySelected', () => {
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      STAGE_SCREEN,
-      {
-        section: 'people',
-        subsection: 'self',
-        firstItem: 0,
-        enableBackButton: false,
-        noNav: true,
-        questionText,
-        orgId,
-        contactId: myId,
-      },
-      { stage, contactId: myId, orgId, isAlreadySelected: true },
-    );
-
-    expect(navigatePush).toHaveBeenCalledWith(CELEBRATION_SCREEN, {
-      contactId: myId,
-      orgId,
-    });
-  });
-});
-
-describe('StageScreen, not isAlreadySelected', () => {
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      STAGE_SCREEN,
-      {
-        section: 'people',
-        subsection: 'self',
-        firstItem: 0,
-        enableBackButton: false,
-        noNav: true,
-        questionText,
-        orgId,
-        contactId: myId,
-      },
-      { stage, contactId: myId, orgId, isAlreadySelected: false },
-    );
-
-    expect(navigatePush).toHaveBeenCalledWith(SELECT_MY_STEP_SCREEN, {
-      contactId: myId,
-      enableBackButton: true,
-      contactStage: stage,
-      organization: { id: orgId },
-    });
-  });
-});
-
-describe('PersonStageScreen, isAlreadySelected', () => {
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      PERSON_STAGE_SCREEN,
-      {
-        section: 'people',
-        subsection: 'person',
-        firstItem: 0,
-        enableBackButton: false,
-        noNav: true,
-        questionText,
-        orgId,
-        contactId: otherId,
-        contactAssignmentId,
-        name: otherName,
-      },
-      {
-        stage,
-        contactId: otherId,
-        name: otherName,
-        orgId,
-        isAlreadySelected: true,
-      },
-    );
-
-    expect(navigatePush).toHaveBeenCalledWith(CELEBRATION_SCREEN, {
-      contactId: otherId,
-      orgId,
-    });
-  });
-});
-
-describe('PersonStageScreen, not isAlreadySelected', () => {
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      PERSON_STAGE_SCREEN,
-      {
-        section: 'people',
-        subsection: 'person',
-        firstItem: 0,
-        enableBackButton: false,
-        noNav: true,
-        questionText,
-        orgId,
-        contactId: otherId,
-        contactAssignmentId,
-        name: otherName,
-      },
-      {
-        stage,
-        contactId: otherId,
-        name: otherName,
-        orgId,
-        isAlreadySelected: false,
-      },
-    );
-
-    expect(navigatePush).toHaveBeenCalledWith(PERSON_SELECT_STEP_SCREEN, {
-      contactStage: stage,
-      contactId: otherId,
-      organization: { id: orgId },
-      contactName: otherName,
-      createStepTracking: buildTrackingObj(
-        'people : person : steps : create',
-        'people',
-        'person',
-        'steps',
-      ),
-    });
-  });
-});
-
-describe('SelectMyStepScreen next', () => {
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      SELECT_MY_STEP_SCREEN,
-      {
-        enableBackButton: true,
-        contactStage: stage,
-        organization: { id: orgId },
-      },
-      {},
-    );
-
-    expect(navigatePush).toHaveBeenCalledWith(CELEBRATION_SCREEN, {});
-  });
-});
-
-describe('PersonSelectStepScreen next', () => {
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      PERSON_SELECT_STEP_SCREEN,
-      {
-        contactStage: stage,
-        contactId: otherId,
-        organization: { id: orgId },
-        contactName: otherName,
-        createStepTracking: buildTrackingObj(
-          'people : person : steps : create',
-          'people',
-          'person',
-          'steps',
-        ),
-      },
-      {},
-    );
-
-    expect(navigatePush).toHaveBeenCalledWith(CELEBRATION_SCREEN, {});
-  });
-});
-
-describe('CelebrationScreen next', () => {
-  const reloadJourneyResponse = { type: 'reload journey' };
-  const popToTopResponse = { type: 'pop to top of stack' };
-  const navigateBackResponse = { type: 'navigate back' };
-
-  beforeEach(() => {
-    reactNavigation.StackActions.popToTop = jest
-      .fn()
-      .mockReturnValue(popToTopResponse);
-    navigateBack.mockReturnValue(navigateBackResponse);
-    reloadJourney.mockReturnValue(reloadJourneyResponse);
-  });
-
-  it('should fire required next actions', async () => {
-    await buildAndCallNext(
-      CELEBRATION_SCREEN,
-      { contactId: myId, orgId },
-      { contactId: myId, orgId },
-    );
-
-    expect(reloadJourney).toHaveBeenCalledWith(myId, orgId);
-    expect(reactNavigation.StackActions.popToTop).toHaveBeenCalledTimes(1);
-    expect(navigateBack).toHaveBeenCalledTimes(1);
   });
 });
