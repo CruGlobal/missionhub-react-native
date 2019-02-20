100dd7586c434d9d0c4ee85a31789d00b6c553fa
dsgoers <daniel.goers@cru.org>
2019-02-20 09:29:33 -0500

    Fix a test

--- src/actions/__tests__/navigation.js
+++ src/actions/__tests__/navigation.js
@@ -34,7 +34,7 @@ describe('navigatePush', () => {
     navigatePush(routeName)(jest.fn());
     expect(NavigationActions.navigate).toHaveBeenCalledWith({
       routeName,
-      params,
+      params: {},
     });
   });
 });
@@ -69,7 +69,7 @@ describe('navigateReset', () => {
     navigateReset(routeName)(jest.fn());
     expect(NavigationActions.navigate).toHaveBeenCalledWith({
       routeName,
-      params,
+      params: {},
     });
     expect(StackActions.reset).toHaveBeenCalledWith({
       index: 0,
