b196f144f72d2ad41c9fb3ffe4e9b3e2c4fb35a1
Robert Eldredge <robert.eldredge@cru.org>
2019-02-20 14:16:51 -0500

    start AcceptedStepItem

---
+++ src/components/AcceptedStepItem/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { Text, Card } from '../common';
+import { navigatePush } from '../../actions/navigation';
+import { STEP_DETAIL_SCREEN } from '../../containers/StepDetailScreen';
+
+import styles from './styles';
+
+class StepSuggestionItem extends Component {
+  handlePress = () => {
+    const { dispatch, step } = this.props;
+    dispatch(navigatePush(STEP_DETAIL_SCREEN, { step }));
+  };
+
+  render() {
+    const {
+      step: { body },
+    } = this.props;
+
+    return (
+      <Card onPress={this.handlePress} style={styles.card}>
+        <Text style={styles.stepText}>{body}</Text>
+      </Card>
+    );
+  }
+}
+
+StepSuggestionItem.propTypes = {
+  step: PropTypes.shape({
+    body: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default connect()(StepSuggestionItem);
