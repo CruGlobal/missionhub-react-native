91071706e4e348e68b393bba247e3477f1601694
Robert Eldredge <robert.eldredge@cru.org>
2019-02-20 14:17:21 -0500

    Also AcceptedStepItem

--- src/components/AcceptedStepItem/index.js
+++ src/components/AcceptedStepItem/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { Text, Card } from '../common';
+import { View, Text, Card, IconButton } from '../common';
 import { navigatePush } from '../../actions/navigation';
 import { STEP_DETAIL_SCREEN } from '../../containers/StepDetailScreen';
 
 import styles from './styles';
+import Icon from '../Icon/index';
 
-class StepSuggestionItem extends Component {
+class AcceptedStepItem extends Component {
   handlePress = () => {
     const { dispatch, step } = this.props;
     dispatch(navigatePush(STEP_DETAIL_SCREEN, { step }));
@@ -21,16 +22,27 @@ class StepSuggestionItem extends Component {
 
     return (
       <Card onPress={this.handlePress} style={styles.card}>
-        <Text style={styles.stepText}>{body}</Text>
+        <View flex={1} flexDirection={'row'}>
+          <View flex={1} flexDirection={'column'}>
+            <Text>REMINDER</Text>
+            <Text style={styles.stepText}>{body}</Text>
+          </View>
+          <IconButton
+            style={styles.completeIcon}
+            name="deleteIcon"
+            type="MissionHub"
+            onPress={this.handlePress}
+          />
+        </View>
       </Card>
     );
   }
 }
 
-StepSuggestionItem.propTypes = {
+AcceptedStepItem.propTypes = {
   step: PropTypes.shape({
     body: PropTypes.string.isRequired,
   }).isRequired,
 };
 
-export default connect()(StepSuggestionItem);
+export default connect()(AcceptedStepItem);
