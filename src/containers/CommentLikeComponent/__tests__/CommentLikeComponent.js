f99e6c830daebe72cace9722585f8ad0837abae9
dsgoers <daniel.goers@cru.org>
2019-02-21 15:06:28 -0500

    Add more tests

--- src/containers/CommentLikeComponent/__tests__/CommentLikeComponent.js
+++ src/containers/CommentLikeComponent/__tests__/CommentLikeComponent.js
@@ -10,16 +10,50 @@ import { renderShallow } from '../../../../testUtils';
 const mockStore = configureStore([thunk]);
 let store;
 
-const event = {};
+const myId = '2342';
 
 beforeEach(() => {
   jest.clearAllMocks();
 
-  store = mockStore({ auth: { person: { id: '2342' } } });
+  store = mockStore({ auth: { person: { id: myId } } });
 });
 
 it('renders nothing with no subject person', () => {
   expect(
-    renderShallow(<CommentLikeComponent event={event} />, store),
+    renderShallow(<CommentLikeComponent event={{}} />, store),
   ).toMatchSnapshot();
+});
+
+describe('with subject person', () => {
+  const event = {
+    subject_person: {},
+    likes_count: 54,
+    comments_count: 15,
+  };
+
+  it('renders for me', () => {
+    expect(
+      renderShallow(<CommentLikeComponent event={event} />, store),
+    ).toMatchSnapshot();
+  });
+
+  it('renders for someone else', () => {
+    expect(
+      renderShallow(
+        <CommentLikeComponent
+          event={{ ...event, subject_person: { id: myId } }}
+        />,
+        store,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('renders when liked', () => {
+    expect(
+      renderShallow(
+        <CommentLikeComponent event={{ ...event, liked: true }} />,
+        store,
+      ),
+    ).toMatchSnapshot();
+  });
 });
