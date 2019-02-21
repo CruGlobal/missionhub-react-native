13b0a4788fa275845538e56d9d11ea0ff3c144d5
dsgoers <daniel.goers@cru.org>
2019-02-21 14:25:11 -0500

    Add another test

--- src/reducers/__tests__/celebrateComments.js
+++ src/reducers/__tests__/celebrateComments.js
@@ -1,23 +1,90 @@
 import { REQUESTS } from '../../actions/api';
 import celebrateCommentsReducer from '../celebrateComments';
+import { getPagination } from '../../utils/common';
+
+jest.mock('../../utils/common');
 
 beforeEach(() => jest.clearAllMocks());
 
-describe('REQUESTS.GET_CELEBRATE_COMMENTS.SUCCESS', () => {
-  it('should add object to initial state', () => {
-    const eventId = '13407923';
-    const response = [{ id: 'comment one' }, { id: 'comment two' }];
+const getPaginationResult = { page: 4 };
+getPagination.mockReturnValue(getPaginationResult);
 
-    expect(
-      celebrateCommentsReducer(undefined, {
-        type: REQUESTS.GET_CELEBRATE_COMMENTS.SUCCESS,
-        results: { response },
-        query: { eventId },
-      }),
-    ).toEqual({
+describe('REQUESTS.GET_CELEBRATE_COMMENTS.SUCCESS', () => {
+  const eventId = '13407923';
+  const response = [{ id: 'comment one' }, { id: 'comment two' }];
+
+  const baseAction = {
+    type: REQUESTS.GET_CELEBRATE_COMMENTS.SUCCESS,
+    results: { response },
+    query: { eventId },
+  };
+
+  describe('initial state', () => {
+    it('should add object to initial state', () => {
+      expect(celebrateCommentsReducer(undefined, baseAction)).toEqual({
+        all: {
+          [eventId]: { comments: response, pagination: expect.anything() },
+        },
+      });
+    });
+  });
+
+  describe('state has existing comments', () => {
+    const existingComments = [{ id: 'comment three' }, { id: 'comment four' }];
+
+    it('should add objects from next page', () => {
+      expect(
+        celebrateCommentsReducer(
+          {
+            all: {
+              [eventId]: { comments: existingComments },
+            },
+          },
+          {
+            ...baseAction,
+            query: { ...baseAction.query, page: {} },
+          },
+        ),
+      ).toEqual({
+        all: {
+          [eventId]: {
+            comments: [...existingComments, ...response],
+            pagination: expect.anything(),
+          },
+        },
+      });
+    });
+
+    it('should reset comments if there is no page', () => {
+      expect(
+        celebrateCommentsReducer(
+          {
+            all: {
+              [eventId]: { comments: existingComments },
+            },
+          },
+          baseAction,
+        ),
+      ).toEqual({
+        all: {
+          [eventId]: {
+            comments: response,
+            pagination: expect.anything(),
+          },
+        },
+      });
+    });
+  });
+
+  it('should update pagination data', () => {
+    expect(celebrateCommentsReducer(undefined, baseAction)).toEqual({
       all: {
-        [eventId]: { comments: response, pagination: expect.anything() },
+        [eventId]: {
+          comments: expect.anything(),
+          pagination: getPaginationResult,
+        },
       },
     });
+    expect(getPagination).toHaveBeenCalledWith(baseAction, response.length);
   });
 });
