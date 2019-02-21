c3bb2ac008e493279d384663a1bb141c289238c9
dsgoers <daniel.goers@cru.org>
2019-02-21 13:32:27 -0500

    Add another test

--- src/actions/__tests__/celebrateComments.js
+++ src/actions/__tests__/celebrateComments.js
@@ -1,20 +1,49 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import { reloadCelebrateComments } from '../celebrateComments';
+import { DEFAULT_PAGE_LIMIT } from '../../constants';
+import {
+  getCelebrateCommentsNextPage,
+  reloadCelebrateComments,
+} from '../celebrateComments';
 import callApi, { REQUESTS } from '../api';
+import { celebrateCommentsSelector } from '../../selectors/celebrateComments';
 
 jest.mock('../api');
+jest.mock('../../selectors/celebrateComments');
 
 const event = { id: '80890', organization: { id: '645654' } };
+const comment = { pagination: { page: 2, hasNextPage: true } };
 const callApiResponse = { result: 'hello world' };
+const celebrateComments = { someProp: 'asdfasdfasdf' };
 
 const mockStore = configureStore([thunk]);
 let store;
 
 callApi.mockReturnValue(() => callApiResponse);
+celebrateCommentsSelector.mockReturnValue(comment);
 
-beforeEach(() => (store = mockStore()));
+beforeEach(() => (store = mockStore({ celebrateComments })));
+
+describe('getCelebrateCommentsNextPage', () => {
+  it('should send request with next page', async () => {
+    const result = await store.dispatch(getCelebrateCommentsNextPage(event));
+
+    expect(celebrateCommentsSelector).toHaveBeenCalledWith(
+      { celebrateComments },
+      { eventId: event.id },
+    );
+    expect(result).toEqual(callApiResponse);
+    expect(callApi).toHaveBeenCalledWith(REQUESTS.GET_CELEBRATE_COMMENTS, {
+      orgId: event.organization.id,
+      eventId: event.id,
+      page: {
+        limit: DEFAULT_PAGE_LIMIT,
+        offset: DEFAULT_PAGE_LIMIT * comment.pagination.page,
+      },
+    });
+  });
+});
 
 describe('reloadCelebrateComments', () => {
   it('should send request with no page', async () => {
