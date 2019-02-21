88943f8d0f34c583e0b56689a5d6a271ec57ea7b
dsgoers <daniel.goers@cru.org>
2019-02-21 16:46:20 -0500

    Add more tests

---
+++ src/components/CardTime/__tests__/CardTime.js
@@ -0,0 +1,12 @@
+import 'react-native';
+import React from 'react';
+
+import CardTime from '..';
+
+import { testSnapshotShallow } from '../../../../testUtils';
+
+const date = '2017-10-09T13:51:49.888Z';
+
+it('renders correctly', () => {
+  testSnapshotShallow(<CardTime date={date} />);
+});
--- src/components/CardTime/index.js
+++ src/components/CardTime/index.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { DateComponent } from '../../components/common';
 
 import styles from './styles';
 
-export default class CardTime extends Component {
-  render() {
-    const { date } = this.props;
-    return <DateComponent style={styles.time} date={date} format={'LT'} />;
-  }
+export default function CardTime({ date }) {
+  return <DateComponent style={styles.time} date={date} format={'LT'} />;
 }
 
 CardTime.propTypes = {
