999d1fb87797b0f670fb750ff8c2f8744982ef4d
dsgoers <daniel.goers@cru.org>
2019-02-25 10:29:49 -0500

    Refactor a test

--- src/components/StepDetailScreen/__tests__/StepDetailScreen.js
+++ src/components/StepDetailScreen/__tests__/StepDetailScreen.js
@@ -1,36 +1,41 @@
 import React from 'react';
 
-import { testSnapshotShallow } from '../../../../testUtils/index';
+import { renderShallow } from '../../../../testUtils/index';
 import StepDetailScreen from '../index';
+
+let bottomButtonProps;
+let screen;
 
 beforeEach(() => {
   jest.clearAllMocks();
+
+  screen = renderShallow(
+    <StepDetailScreen
+      text="Roge is well behaved"
+      CenterHeader={{ prop: 'center header' }}
+      RightHeader={{ prop: 'right header' }}
+      Body={null}
+      bottomButtonProps={bottomButtonProps}
+    />,
+  );
 });
 
 describe('bottomButtonProps are not null', () => {
+  beforeAll(() => {
+    bottomButtonProps = { text: 'bottom button props', onPress: () => {} };
+  });
+
   it('renders correctly', () => {
-    testSnapshotShallow(
-      <StepDetailScreen
-        text="Roge is well behaved"
-        CenterHeader={{ prop: 'center header' }}
-        RightHeader={{ prop: 'right header' }}
-        Body={null}
-        bottomButtonProps={{ text: 'bottom button props', onPress: () => {} }}
-      />,
-    );
+    expect(screen).toMatchSnapshot();
   });
 });
 
 describe('bottomButtonProps are null', () => {
+  beforeAll(() => {
+    bottomButtonProps = null;
+  });
+
   it('renders correctly', () => {
-    testSnapshotShallow(
-      <StepDetailScreen
-        text="Roge is well behaved"
-        CenterHeader={{ prop: 'center header' }}
-        RightHeader={{ prop: 'right header' }}
-        Body={{ prop: 'body' }}
-        bottomButtonProps={null}
-      />,
-    );
+    expect(screen).toMatchSnapshot();
   });
 });
