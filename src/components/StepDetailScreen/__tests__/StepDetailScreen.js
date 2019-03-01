f96117531a63ccd1726d8ff3440528b7bd337e76
Robert Eldredge <robert.eldredge@cru.org>
2019-03-01 10:28:25 -0500

    update tests

--- src/components/StepDetailScreen/__tests__/StepDetailScreen.js
+++ src/components/StepDetailScreen/__tests__/StepDetailScreen.js
@@ -21,6 +21,17 @@ beforeEach(() => {
   );
 });
 
+describe('test markdown styles', () => {
+  beforeAll(() => {
+    markdown =
+      '# HEADING 1\r\n## HEADING 2\r\n### HEADING 3\r\nSample text: **bold** and *italic*\r\n\r\n- Bullet 1\r\n- Bullet 2\r\n- Bullet 3\r\n1. Bullet 1\r\n2. Bullet 2\r\n3. Bullet 3\r\n\r\n---\r\n\r\n![Image](https://s3.amazonaws.com/stage.missionhub.com/organizations/community_photos/original/15853/1549383219755.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJKEW3SYT6MSDBAPA%2F20190227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20190227T160524Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=b1349c8dbc71bfa2af6bfcc953db10edeac258208ddb0b4fdf5905d0f7a25267)\r\n\r\n---\r\n\r\n[Get MissionHub!](get.missionhub.com)\r\n\r\n> *Some Quote* \n> *~ Some Guy*\r\n';
+  });
+
+  it('renders correctly', () => {
+    expect(screen).toMatchSnapshot();
+  });
+});
+
 describe('markdown is not null', () => {
   beforeAll(() => {
     markdown = 'ROBERT ROBERT ROBERT';
--- src/components/StepDetailScreen/index.js
+++ src/components/StepDetailScreen/index.js
@@ -20,10 +20,7 @@ export default function StepDetailScreen({
   RightHeader,
   bottomButtonProps,
 }) {
-  const { container, stepTitleText, bodyStyle, backButton } = styles;
-
-  const testMarkdown =
-    '# HEADING 1\r\n## HEADING 2\r\n### HEADING 3\r\nSample text: **bold** and *italic*\r\n\r\n- Bullet 1\r\n- Bullet 2\r\n- Bullet 3\r\n1. Bullet 1\r\n2. Bullet 2\r\n3. Bullet 3\r\n\r\n---\r\n\r\n![Image](https://s3.amazonaws.com/stage.missionhub.com/organizations/community_photos/original/15853/1549383219755.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJKEW3SYT6MSDBAPA%2F20190227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20190227T160524Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=b1349c8dbc71bfa2af6bfcc953db10edeac258208ddb0b4fdf5905d0f7a25267)\r\n\r\n---\r\n\r\n[Get MissionHub!](get.missionhub.com)\r\n\r\n> *Some Quote* \n> *~ Some Guy*\r\n';
+  const { container, stepTitleText, backButton } = styles;
 
   return (
     <View flex={1} style={container}>
@@ -39,7 +36,7 @@ export default function StepDetailScreen({
       <View flex={1}>
         {markdown && (
           <ScrollView style={styles.body}>
-            <Markdown style={markdownTheme}>{testMarkdown}</Markdown>
+            <Markdown style={markdownTheme}>{markdown}</Markdown>
           </ScrollView>
         )}
       </View>
