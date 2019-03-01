6eb235208d0172cfc57527ea5190369644577d6e
dsgoers <daniel.goers@cru.org>
2019-03-01 11:09:18 -0500

    Update test

--- src/utils/__tests__/steps.js
+++ src/utils/__tests__/steps.js
@@ -1,13 +1,21 @@
+import uuidv4 from 'uuid/v4';
+
 import { CUSTOM_STEP_TYPE } from '../../constants';
 import { buildCustomStep } from '../steps';
 
+jest.mock('uuid/v4');
+
 const text = 'roge rules';
 const self_step = true;
+const id = 'f6c41d33-a507-49c4-bc12-34d0dad028bd';
+const locale = 'es';
+
+uuidv4.mockReturnValue(id);
 
 describe('buildCustomStep', () => {
   it('creates a custom step', () => {
     expect(buildCustomStep(text, self_step)).toEqual({
-      id: expect.any(String),
+      id,
       body: text,
       selected: true,
       locale: undefined,
