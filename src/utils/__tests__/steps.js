7f1a8b7b806c1f3e445d2945f01456ac7e157be2
dsgoers <daniel.goers@cru.org>
2019-03-01 11:10:44 -0500

    Update test

--- src/utils/__tests__/steps.js
+++ src/utils/__tests__/steps.js
@@ -1,9 +1,11 @@
 import uuidv4 from 'uuid/v4';
+import i18next from 'i18next';
 
 import { CUSTOM_STEP_TYPE } from '../../constants';
 import { buildCustomStep } from '../steps';
 
 jest.mock('uuid/v4');
+jest.mock('i18next');
 
 const text = 'roge rules';
 const self_step = true;
@@ -11,6 +13,7 @@ const id = 'f6c41d33-a507-49c4-bc12-34d0dad028bd';
 const locale = 'es';
 
 uuidv4.mockReturnValue(id);
+i18next.language = locale;
 
 describe('buildCustomStep', () => {
   it('creates a custom step', () => {
@@ -18,7 +21,7 @@ describe('buildCustomStep', () => {
       id,
       body: text,
       selected: true,
-      locale: undefined,
+      locale,
       challenge_type: CUSTOM_STEP_TYPE,
       self_step,
     });
