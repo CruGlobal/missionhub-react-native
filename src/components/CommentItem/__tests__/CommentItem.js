242d54956cecc776fca239eabe6952af1ef9ae92
dsgoers <daniel.goers@cru.org>
2019-03-05 09:32:58 -0500

    Update tests

--- src/components/CommentItem/__tests__/CommentItem.js
+++ src/components/CommentItem/__tests__/CommentItem.js
@@ -12,25 +12,39 @@ const item = {
   person: { first_name: 'Roge', last_name: 'Goers' },
 };
 
+const organization = { id: 'w342342' };
+
 it('renders correctly', () => {
-  testSnapshotShallow(<CommentItem item={item} />);
+  testSnapshotShallow(<CommentItem item={item} organization={organization} />);
 });
 
 it('renders correctly with onLongPress', () => {
-  testSnapshotShallow(<CommentItem item={item} onLongPress={jest.fn()} />);
+  testSnapshotShallow(
+    <CommentItem
+      item={item}
+      onLongPress={jest.fn()}
+      organization={organization}
+    />,
+  );
 });
 
 it('renders correctly with onLongPress', () => {
   const onLongPress = jest.fn();
   const component = shallow(
-    <CommentItem item={item} onLongPress={onLongPress} />,
+    <CommentItem
+      item={item}
+      onLongPress={onLongPress}
+      organization={organization}
+    />,
   );
   component.props().onLongPress();
   expect(onLongPress).toHaveBeenCalledWith(item, undefined);
 });
 
 it('calls ref', () => {
-  const instance = shallow(<CommentItem item={item} />).instance();
+  const instance = shallow(
+    <CommentItem item={item} organization={organization} />,
+  ).instance();
   instance.view = null;
   instance.ref('test');
   expect(instance.view).toEqual('test');
