b1eaf3a75ac6f9c5997f53eaf745fbed39da0b31
dsgoers <daniel.goers@cru.org>
2019-03-05 09:46:09 -0500

    Refactor tests

--- src/components/CommentItem/__tests__/CommentItem.js
+++ src/components/CommentItem/__tests__/CommentItem.js
@@ -1,10 +1,11 @@
 import 'react-native';
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import CommentItem from '..';
 
-import { testSnapshotShallow } from '../../../../testUtils';
+Enzyme.configure({ adapter: new Adapter() });
 
 const item = {
   content: 'hello roge',
@@ -16,31 +17,29 @@ const organization = { id: '7342342' };
 
 let onLongPress;
 
+let screen;
+
+beforeEach(() => {
+  screen = shallow(
+    <CommentItem
+      item={item}
+      organization={organization}
+      onLongPress={onLongPress}
+    />,
+  );
+});
+
 describe('with onLongPress', () => {
   beforeAll(() => {
     onLongPress = jest.fn();
   });
 
   it('renders correctly with onLongPress', () => {
-    testSnapshotShallow(
-      <CommentItem
-        item={item}
-        onLongPress={onLongPress}
-        organization={organization}
-      />,
-    );
+    expect(screen).toMatchSnapshot();
   });
 
   it('calls onLongPress', () => {
-    const component = shallow(
-      <CommentItem
-        item={item}
-        onLongPress={onLongPress}
-        organization={organization}
-      />,
-    );
-
-    component.props().onLongPress();
+    screen.props().onLongPress();
 
     expect(onLongPress).toHaveBeenCalledWith(item, undefined);
   });
@@ -52,23 +51,11 @@ describe('without onLongPress', () => {
   });
 
   it('renders correctly', () => {
-    testSnapshotShallow(
-      <CommentItem
-        item={item}
-        organization={organization}
-        onLongPress={onLongPress}
-      />,
-    );
+    expect(screen).toMatchSnapshot();
   });
 
   it('calls ref', () => {
-    const instance = shallow(
-      <CommentItem
-        item={item}
-        organization={organization}
-        onLongPress={onLongPress}
-      />,
-    ).instance();
+    const instance = screen.instance();
     instance.view = null;
     instance.ref('test');
     expect(instance.view).toEqual('test');
