e313b42f486264f8e0673c653ea55134e2a39c29
Daniel Goers <daniel.goers@Cru.org>
2019-03-14 14:05:09 -0400

    Mhp 2461 (#849)
    * Track an action when a celebrate comment is added
    
    * Update state tracking data for celebrate detail screen

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -442,7 +442,11 @@ const screens = {
   [COMPLETE_STEP_FLOW]: CompleteStepFlowNavigator,
   [CELEBRATE_DETAIL_SCREEN]: buildTrackedScreen(
     CelebrateDetailScreen,
-    buildTrackingObj('community : celebrate : detail', 'celebrate : detail'),
+    buildTrackingObj(
+      'communities : celebration : comment',
+      'communities',
+      'celebration',
+    ),
     { gesturesEnabled: true },
   ),
   [EDIT_COMMENT_SCREEN]: buildTrackedScreen(
