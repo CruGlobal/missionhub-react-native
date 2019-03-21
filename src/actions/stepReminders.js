8f318c6cfaa14a62b644fc074966e5e5d073904c
dsgoers <daniel.goers@cru.org>
2019-03-21 16:21:47 -0400

    wip

--- src/actions/stepReminders.js
+++ src/actions/stepReminders.js
@@ -1,29 +1,4 @@
-import i18next from 'i18next';
-
-import {
-  TOGGLE_STEP_FOCUS,
-  COMPLETED_STEP_COUNT,
-  STEP_NOTE,
-  ACTIONS,
-  DEFAULT_PAGE_LIMIT,
-  ACCEPTED_STEP,
-} from '../constants';
-import {
-  buildTrackingObj,
-  formatApiDate,
-  getAnalyticsSubsection,
-  isCustomStep,
-} from '../utils/common';
-import {
-  COMPLETE_STEP_FLOW,
-  COMPLETE_STEP_FLOW_NAVIGATE_BACK,
-} from '../routes/constants';
-
-import { refreshImpact } from './impact';
-import { navigatePush } from './navigation';
 import callApi, { REQUESTS } from './api';
-import { trackAction, trackStepAdded } from './analytics';
-import { reloadGroupCelebrateFeed } from './celebration';
 
 export function createStepReminder(challenge_id, at, recurrence) {
   return dispatch => {
@@ -42,9 +17,6 @@ export function createStepReminder(challenge_id, at, recurrence) {
         },
       },
     };
-
-    console.log(challenge_id);
-    console.log(payload);
 
     return dispatch(
       callApi(REQUESTS.CREATE_CHALLENGE_REMINDER, { challenge_id }, payload),
--- src/components/DatePicker/index.ios.js
+++ src/components/DatePicker/index.ios.js
@@ -64,7 +64,6 @@ class MyDatePickerIOS extends Component {
   }
 
   onDateChange = date => {
-    console.log(date);
     this.setState({
       allowPointerEvents: false,
       date: date,
