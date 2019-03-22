393071108b73e4f5deeab6c2fc36c99f9d7f38d9
dsgoers <daniel.goers@cru.org>
2019-03-22 15:58:37 -0400

    Refactor

--- src/actions/__tests__/stepReminders.js
+++ src/actions/__tests__/stepReminders.js
@@ -12,9 +12,9 @@ const { ONCE, DAILY, WEEKLY, MONTHLY } = REMINDER_RECURRENCES;
 const mockStore = configureStore([thunk]);
 
 const challenge_id = '442324';
-const at = new Date('2019-3-21 15:45:32');
 const callApiResponse = { type: 'called api' };
 
+let at;
 let recurrence;
 let store;
 
@@ -26,69 +26,75 @@ beforeEach(() => {
 });
 
 describe('createStepReminder', () => {
+  beforeAll(() => (at = new Date('2019-3-21 15:45:32')));
+
   beforeEach(() =>
     store.dispatch(createStepReminder(challenge_id, at, recurrence)));
 
-  const testApiCall = attributes =>
+  const testApiCall = (type, at, on) =>
     expect(callApi).toHaveBeenCalledWith(
       REQUESTS.CREATE_CHALLENGE_REMINDER,
       { challenge_id },
       {
         data: {
-          attributes,
+          attributes: { type, at, on },
         },
       },
     );
 
-  it('dispatches result to store', () => {
-    expect(store.getActions()).toEqual([callApiResponse]);
-  });
+  it('dispatches result to store', () =>
+    expect(store.getActions()).toEqual([callApiResponse]));
 
   describe('with once recurrence', () => {
     beforeAll(() => (recurrence = ONCE));
 
-    it('calls api with correct payload', () => {
-      testApiCall({
-        type: ONCE,
-        at: at.toISOString(),
-        on: undefined,
-      });
-    });
+    it('calls api with correct payload', () =>
+      testApiCall(ONCE, at.toISOString(), undefined));
   });
 
   describe('with daily recurrence', () => {
     beforeAll(() => (recurrence = DAILY));
 
-    it('calls api with correct payload', () => {
-      testApiCall({
-        type: DAILY,
-        at: at.toLocaleTimeString(undefined, { hour12: false }),
-        on: undefined,
-      });
-    });
+    it('calls api with correct payload', () =>
+      testApiCall(
+        DAILY,
+        at.toLocaleTimeString(undefined, { hour12: false }),
+        undefined,
+      ));
   });
 
   describe('with weekly recurrence', () => {
     beforeAll(() => (recurrence = WEEKLY));
 
-    it('calls api with correct payload', () => {
-      testApiCall({
-        type: WEEKLY,
-        at: at.toLocaleTimeString(undefined, { hour12: false }),
-        on: DAYS_OF_THE_WEEK[4],
-      });
-    });
+    it('calls api with correct payload', () =>
+      testApiCall(
+        WEEKLY,
+        at.toLocaleTimeString(undefined, { hour12: false }),
+        DAYS_OF_THE_WEEK[4],
+      ));
   });
 
   describe('with monthly recurrence', () => {
     beforeAll(() => (recurrence = MONTHLY));
 
-    it('calls api with correct payload', () => {
-      testApiCall({
-        type: MONTHLY,
-        at: at.toLocaleTimeString(undefined, { hour12: false }),
-        on: at.getDate(),
-      });
+    describe('with day that occurs in every month', () => {
+      it('calls api with correct payload', () =>
+        testApiCall(
+          MONTHLY,
+          at.toLocaleTimeString(undefined, { hour12: false }),
+          at.getDate(),
+        ));
+    });
+
+    describe('with day that does not occur in every month', () => {
+      beforeAll(() => (at = new Date('2019-3-30 15:45:32')));
+
+      it('calls api with correct payload', () =>
+        testApiCall(
+          MONTHLY,
+          at.toLocaleTimeString(undefined, { hour12: false }),
+          at.getDate() - 32,
+        ));
     });
   });
 });
