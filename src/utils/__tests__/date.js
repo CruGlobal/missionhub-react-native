5633becd7e4d94be57183fbc5880be4e351beefb
Robert Eldredge <robert.eldredge@cru.org>
2019-03-26 11:28:41 -0400

    begin work on reminder to date conversion

--- src/utils/__tests__/date.js
+++ src/utils/__tests__/date.js
@@ -1,11 +1,14 @@
 import moment from 'moment';
 import MockDate from 'mockdate';
 
-import { getDate, modeIs24Hour } from '../date';
+import { getDate, modeIs24Hour, reminderToDate } from '../date';
+import { DAYS_OF_THE_WEEK, REMINDER_RECURRENCES } from '../../constants';
 
-const mockDate = '2018-09-12 12:00:00 PM GMT+0';
-const mockMinDate = '2019-10-13 12:00:00 PM GMT+0';
-const mockMaxDate = '2017-08-10 11:00:00 PM GMT+0';
+const { ONCE, DAILY, WEEKLY, MONTHLY } = REMINDER_RECURRENCES;
+
+const mockDate = '2018-09-12T12:00:00+00:00';
+const mockMinDate = '2019-10-13T12:00:00+00:00';
+const mockMaxDate = '2017-08-10T12:00:00+00:00';
 MockDate.set(mockDate);
 
 describe('getDate', () => {
@@ -42,5 +45,53 @@ describe('modeIs24Hour', () => {
 
   it('format has neither h nor a', () => {
     expect(modeIs24Hour('HH:mm')).toEqual(true);
+  });
+});
+
+describe('reminderToDate', () => {
+  let reminder;
+
+  describe('once', () => {
+    it('returns date as is', () => {
+      reminder = { type: ONCE, at: '2018-10-23T12:00:00+00:00' };
+
+      expect(reminderToDate(reminder)).toEqual('');
+    });
+  });
+
+  describe('daily', () => {
+    it('returns reminder for today', () => {
+      reminder = { type: DAILY, at: '14:00:00' };
+
+      expect(reminderToDate(reminder)).toEqual('');
+    });
+
+    it('returns reminder for tomorrow', () => {
+      reminder = { type: DAILY, at: '10:00:00' };
+
+      expect(reminderToDate(reminder)).toEqual('');
+    });
+  });
+
+  describe('weekly', () => {
+    it('returns reminder for this week', () => {
+      reminder = {
+        type: WEEKLY,
+        at: '14:00:00',
+        on: DAYS_OF_THE_WEEK[6],
+      };
+
+      expect(reminderToDate(reminder)).toEqual('');
+    });
+
+    it('returns reminder for tomorrow', () => {
+      reminder = {
+        type: WEEKLY,
+        at: '10:00:00',
+        on: DAYS_OF_THE_WEEK[0],
+      };
+
+      expect(reminderToDate(reminder)).toEqual('');
+    });
   });
 });
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,10 @@
 /* eslint max-params: 0 */
 import moment from 'moment';
 
+import { DAYS_OF_THE_WEEK, REMINDER_RECURRENCES } from '../constants';
+
+const { ONCE, DAILY, WEEKLY, MONTHLY } = REMINDER_RECURRENCES;
+
 export function getDate(date, minDate, maxDate, format) {
   if (!date) {
     const now = new Date();
@@ -32,4 +36,48 @@ export function getDate(date, minDate, maxDate, format) {
 
 export function modeIs24Hour(format) {
   return !format.match(/h|a/);
+}
+
+export function reminderToDate({ type, at, on }) {
+  //return date of the next reminder the user can expect
+  switch (type) {
+    case ONCE:
+      return new Date(at);
+    case DAILY:
+      return getDailyReminderDate(type, at);
+    case WEEKLY:
+      return getWeeklyReminderDate(type, at, on);
+    default:
+      return undefined;
+  }
+}
+
+function getDailyReminderDate(type, at) {
+  const today = new Date();
+  const reminderTime = new Date(at);
+  console.log(reminderTime);
+
+  let todayReminder = today;
+  todayReminder.setHours(reminderTime.getHours());
+  todayReminder.setMinutes(reminderTime.getMinutes());
+
+  return todayReminder - today > 0
+    ? todayReminder
+    : todayReminder.getDate() + 1;
+}
+
+function getWeeklyReminderDate(type, at, on) {
+  const today = new Date();
+  const reminderTime = new Date(at);
+
+  const todayIndex = today.getDay();
+  const reminderIndex = DAYS_OF_THE_WEEK.indexOf(on);
+
+  const thisWeekReminder = today.getDate() + (reminderIndex - todayIndex);
+  thisWeekReminder.setHours(reminderTime.getHours());
+  thisWeekReminder.setMinutes(reminderTime.getMinutes());
+
+  return thisWeekReminder - today > 0
+    ? thisWeekReminder
+    : thisWeekReminder.getDate() + 7;
 }
