f4808b7802dea47b9c6f7b07b850f780705cd7bd
Bryan Eaton <bryaneaton13@gmail.com>
2019-03-26 15:34:20 -0400

    Fix date tests.

--- src/components/DateComponent/__tests__/DateComponent.js
+++ src/components/DateComponent/__tests__/DateComponent.js
@@ -1,82 +1,76 @@
 import React from 'react';
-import 'react-native';
+import MockDate from 'mockdate';
+import moment from 'moment';
 
 import DateComponent from '..';
 
 import { testSnapshotShallow, renderShallow } from '../../../../testUtils';
 
-Date.now = jest.fn(() => new Date(Date.UTC(2018, 5, 11, 12, 0, 0)).valueOf());
+MockDate.set(moment('2018-06-11 12:00:00').toDate(), 0);
 
 it('renders correctly', () => {
   testSnapshotShallow(<DateComponent date="2017-11-20" />);
 });
+const testFormat = (date, formattedText, format) => {
+  const component = renderShallow(
+    <DateComponent date={date} format={format} />,
+  );
+
+  const text = component.find('MyText').props().children;
+
+  expect(text).toEqual(formattedText);
+};
 
 describe('relative formatting', () => {
-  const testDateFormat = (date, formattedText) => {
-    const component = renderShallow(
-      <DateComponent date={date} format={'relative'} />,
-    );
-
-    const text = component.find('MyText').props().children;
-
-    expect(text).toEqual(formattedText);
-  };
+  const testDateFormat = (date, format) => testFormat(date, format, 'relative');
 
   it('renders today', () => {
-    testDateFormat('2018-06-11 12:00:00 UTC', 'Today');
+    testDateFormat('2018-06-11 12:00:00', 'Today');
   });
 
   it('renders yesterday', () => {
-    testDateFormat('2018-06-10 12:00:00 UTC', 'Yesterday');
+    testDateFormat('2018-06-10 12:00:00', 'Yesterday');
   });
 
   it('renders date from last week', () => {
-    testDateFormat('2018-06-09 12:00:00 UTC', 'Saturday');
-    testDateFormat('2018-06-04 12:00:00 UTC', 'Monday');
+    testDateFormat('2018-06-09 12:00:00', 'Saturday');
+    testDateFormat('2018-06-04 12:00:00', 'Monday');
   });
 
   it('renders from this year', () => {
-    testDateFormat('2018-06-03 12:00:00 UTC', 'Sunday, June 3');
-    testDateFormat('2018-05-23 12:00:00 UTC', 'Wednesday, May 23');
+    testDateFormat('2018-06-03 12:00:00', 'Sunday, June 3');
+    testDateFormat('2018-05-23 12:00:00', 'Wednesday, May 23');
   });
 
   it('renders from before this year', () => {
-    testDateFormat('2017-12-31 12:00:00 UTC', 'Sunday, December 31 2017');
-    testDateFormat('2005-02-14 12:00:00 UTC', 'Monday, February 14 2005');
+    testDateFormat('2017-12-31 12:00:00', 'Sunday, December 31 2017');
+    testDateFormat('2005-02-14 12:00:00', 'Monday, February 14 2005');
   });
 });
 
 describe('comment formatting', () => {
-  const testDateFormat = (date, formattedText) => {
-    const component = renderShallow(
-      <DateComponent date={date} format={'comment'} />,
-    );
-
-    const text = component.find('MyText').props().children;
-
-    expect(text).toEqual(formattedText);
-  };
+  const testDateFormat = (date, format) => testFormat(date, format, 'comment');
 
   it('renders today', () => {
-    testDateFormat('2018-06-11 12:00:00 UTC', '8:00 AM');
+    testDateFormat('2018-06-11 12:00:00', '12:00 PM');
   });
 
   it('renders yesterday', () => {
-    testDateFormat('2018-06-10 12:00:00 UTC', 'Yesterday @ 8:00 AM');
+    testDateFormat('2018-06-10 12:00:00', 'Yesterday @ 12:00 PM');
   });
 
   it('renders date from last week', () => {
-    testDateFormat('2018-06-09 12:00:00 UTC', 'Saturday @ 8:00 AM');
-    testDateFormat('2018-06-07 12:00:00 UTC', 'Thursday @ 8:00 AM');
+    testDateFormat('2018-06-09 12:00:00', 'Saturday @ 12:00 PM');
+    testDateFormat('2018-06-07 12:00:00', 'Thursday @ 12:00 PM');
   });
 
   it('renders from this year', () => {
-    testDateFormat('2018-06-03 12:00:00 UTC', 'June 3 @ 8:00 AM');
-    testDateFormat('2018-05-23 12:00:00 UTC', 'May 23 @ 8:00 AM');
+    testDateFormat('2018-06-03 12:00:00', 'June 3 @ 12:00 PM');
+    testDateFormat('2018-05-23 12:00:00', 'May 23 @ 12:00 PM');
   });
 
   it('renders from before this year', () => {
-    testDateFormat('2017-12-31 12:00:00 UTC', 'December 31, 2017 @ 7:00 AM');
-    testDateFormat('2005-02-14 12:00:00 UTC', 'February 14, 2005 @ 7:00 AM');
+    testDateFormat('2017-12-31 12:00:00', 'December 31, 2017 @ 12:00 PM');
+    testDateFormat('2005-02-14 12:00:00', 'February 14, 2005 @ 12:00 PM');
   });
 });
