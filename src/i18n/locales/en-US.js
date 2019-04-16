3396ae20bda5d5ed2bcb6a85f315cff10931a097
Robert Eldredge <robert.eldredge@cru.org>
2019-04-16 10:21:27 -0400

    Add message for Not Sure stage

--- src/i18n/locales/en-US.js
+++ src/i18n/locales/en-US.js
@@ -505,6 +505,7 @@ export default {
       '{{initiator}} completed a Step of Faith with a {{receiverStage}} person.',
     stepOfFaithUnknownStage:
       '{{initiator}} completed a Step of Faith with someone.',
+    stepOfFaithNotSureStage: '{{initiator}} completed a Step of Faith.',
     interaction: '{{initiator}} had a {{interactionName}}.',
     interactionDecision: '{{initiator}} saw someone make a Personal Decision.',
     addedContact: '{{initiator}} added a {{receiverStage}} person.',
