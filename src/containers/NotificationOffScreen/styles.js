076d92b6afa9ba793d3c342c12661492377aa047
Robert Eldredge <robert.eldredge@cru.org>
2019-05-02 10:18:03 -0400

    update NotificationOffScreen styles and layout

--- src/containers/NotificationOffScreen/styles.js
+++ src/containers/NotificationOffScreen/styles.js
@@ -8,52 +8,45 @@ export default StyleSheet.create({
     backgroundColor: theme.primaryColor,
     paddingHorizontal: 40,
   },
+  imageWrap: {
+    paddingVertical: 50,
+  },
   title: {
     color: theme.secondaryColor,
     fontSize: 32,
     textAlign: 'center',
-    paddingTop: 50,
     paddingBottom: 10,
   },
   text: {
     color: theme.white,
     fontSize: 16,
     textAlign: 'center',
-    paddingBottom: 50,
-  },
-  allowRow: {
-    padding: 8,
-    borderTopWidth: 2,
-    borderTopColor: theme.secondaryColor,
-    borderBottomWidth: 2,
-    borderBottomColor: theme.secondaryColor,
   },
   allowText: {
     flex: 1,
     color: theme.white,
     fontSize: 16,
   },
-  allowButton: {
-    backgroundColor: theme.secondaryColor,
+  buttonWrap: {
+    paddingTop: 50,
+  },
+  button: {
     width: theme.fullWidth - 70,
     margin: 8,
     height: 48,
+  },
+  allowButton: {
+    backgroundColor: theme.secondaryColor,
   },
   notNowButton: {
     backgroundColor: theme.transparent,
     borderWidth: 1,
     borderColor: theme.secondaryColor,
-    width: theme.fullWidth - 70,
-    margin: 8,
-    height: 48,
   },
   buttonText: {
     color: theme.white,
     fontWeight: '500',
     fontSize: 14,
     letterSpacing: 1.5,
-  },
-  allowButtonText: {
-    color: theme.white,
   },
 });
