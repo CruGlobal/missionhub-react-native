9d1d782da519274a9481db2d6d07716be5fa7bbf
Scotty Waggoner <ozzieorca@gmail.com>
2019-06-17 15:53:30 -0700

    MHP-2701: Add authLink and rollbarLink

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,13 +1,59 @@
 import { AsyncStorage } from 'react-native';
-import ApolloClient, {
-  InMemoryCache,
-  NormalizedCacheObject,
-} from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import { PersistentStorage, PersistedData } from 'apollo-cache-persist/types';
 
 import { BASE_URL } from './api/utils';
 import { store } from './store';
+import { UPDATE_TOKEN } from './constants';
+import { rollbar } from './utils/rollbar.config';
+
+const rollbarLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(error =>
+      rollbar.error(`[Apollo GraphQL error]: ${error.message}`, error),
+    );
+  }
+  if (networkError) {
+    rollbar.error(
+      `[Apollo Network error]: ${networkError.message}`,
+      networkError,
+    );
+  }
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: { authorization: `Bearer ${store.getState().auth.token}` },
+  });
+  return (
+    (forward &&
+      forward(operation).map(data => {
+        const sessionHeader = operation
+          .getContext()
+          .response.headers.get('X-MH-Session');
+
+        sessionHeader &&
+          store.dispatch({
+            type: UPDATE_TOKEN,
+            token: sessionHeader,
+          });
+        return data;
+      })) ||
+    null
+  );
+});
+
+const httpLink = new HttpLink({
+  uri: `${BASE_URL}/apis/graphql`,
+  credentials: 'same-origin',
+});
+
+const link = ApolloLink.from([rollbarLink, authLink, httpLink]);
 
 const cache = new InMemoryCache();
 
@@ -19,12 +65,6 @@ persistCache({
 });
 
 export const apolloClient = new ApolloClient({
-  uri: `${BASE_URL}/apis/graphql`,
+  link,
   cache,
-  request: operation => {
-    operation.setContext({
-      headers: { authorization: `Bearer ${store.getState().auth.token}` },
-    });
-    return Promise.resolve();
-  },
 });
