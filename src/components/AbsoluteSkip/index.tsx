2265d24a1e6d2d409dc5686a488f1c93396c1b34
Bryan Eaton <bryaneaton13@gmail.com>
2019-06-18 15:04:50 -0400

    Convert AbsoluteSkip component to typescript and hooks.

---
+++ src/components/AbsoluteSkip/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { SafeAreaView, ViewStyle, TextStyle } from 'react-native';
+import { useTranslation } from 'react-i18next';
+
+import { Button } from '../common';
+
+import styles from './styles';
+
+interface AbsoluteSkipProps {
+  onSkip: Function;
+  style?: ViewStyle;
+  textStyle?: TextStyle;
+}
+
+export default ({ onSkip, style, textStyle }: AbsoluteSkipProps) => {
+  const { t } = useTranslation();
+  return (
+    <SafeAreaView style={styles.skipWrap}>
+      <Button
+        type="transparent"
+        onPress={onSkip}
+        text={t('skip').toUpperCase()}
+        style={[styles.skipBtn, style]}
+        buttonTextStyle={[styles.skipBtnText, textStyle]}
+      />
+    </SafeAreaView>
+  );
+};
