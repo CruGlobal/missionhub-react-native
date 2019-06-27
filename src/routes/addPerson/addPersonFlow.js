e2c687575e47d54b0b1ff7f02caa95054bb95ba1
Robert Eldredge <robert.eldredge@cru.org>
2019-06-27 16:32:48 -0400

    Fix other flows

--- src/routes/addPerson/addPersonFlow.js
+++ src/routes/addPerson/addPersonFlow.js
@@ -2,13 +2,14 @@
 
 import { createStackNavigator, StackActions } from 'react-navigation';
 
-import { PEOPLE_TAB } from '../../constants';
+import { CREATE_STEP, PEOPLE_TAB } from '../../constants';
 import {
   navigatePush,
   navigateBack,
   navigateToMainTabs,
 } from '../../actions/navigation';
 import { getOrganizationMembers } from '../../actions/organizations';
+import { createCustomStep } from '../../actions/steps';
 import AddContactScreen, {
   ADD_CONTACT_SCREEN,
 } from '../../containers/AddContactScreen';
@@ -18,6 +19,10 @@ import PersonStageScreen, {
 import PersonSelectStepScreen, {
   PERSON_SELECT_STEP_SCREEN,
 } from '../../containers/PersonSelectStepScreen';
+import SuggestedStepDetailScreen, {
+  SUGGESTED_STEP_DETAIL_SCREEN,
+} from '../../containers/SuggestedStepDetailScreen';
+import AddStepScreen, { ADD_STEP_SCREEN } from '../../containers/AddStepScreen';
 import { wrapNextAction } from '../helpers';
 import { paramsForStageNavigation } from '../utils';
 import { buildTrackingObj } from '../../utils/common';
@@ -75,7 +80,33 @@ export const AddPersonFlowScreens = onFlowComplete => ({
   ),
   [PERSON_SELECT_STEP_SCREEN]: wrapNextAction(
     PersonSelectStepScreen,
-    ({ orgId }) => dispatch => {
+    ({ receiverId, step, orgId }) =>
+      step
+        ? navigatePush(SUGGESTED_STEP_DETAIL_SCREEN, {
+            step,
+            receiverId,
+            orgId,
+          })
+        : navigatePush(ADD_STEP_SCREEN, {
+            type: CREATE_STEP,
+            personId: receiverId,
+            orgId,
+            trackingObj: buildTrackingObj(
+              'people : person : steps : create',
+              'people',
+              'person',
+              'steps',
+            ),
+          }),
+  ),
+  [SUGGESTED_STEP_DETAIL_SCREEN]: wrapNextAction(
+    SuggestedStepDetailScreen,
+    ({ orgId }) => onFlowComplete({ orgId }),
+  ),
+  [ADD_STEP_SCREEN]: wrapNextAction(
+    AddStepScreen,
+    ({ text, personId, orgId }) => dispatch => {
+      dispatch(createCustomStep(text, personId, orgId));
       dispatch(onFlowComplete({ orgId }));
     },
   ),
--- src/routes/steps/addMyStepFlow.js
+++ src/routes/steps/addMyStepFlow.js
@@ -1,16 +1,50 @@
 import { createStackNavigator } from 'react-navigation';
 
-import { wrapNextScreen } from '../helpers';
+import { CREATE_STEP } from '../../constants';
+import { createCustomStep } from '../../actions/steps';
+import { navigatePush } from '../../actions/navigation';
+import { wrapNextScreen, wrapNextAction } from '../helpers';
+import { buildTrackingObj } from '../../utils/common';
 import SelectMyStepScreen, {
   SELECT_MY_STEP_SCREEN,
 } from '../../containers/SelectMyStepScreen';
+import SuggestedStepDetailScreen, {
+  SUGGESTED_STEP_DETAIL_SCREEN,
+} from '../../containers/SuggestedStepDetailScreen';
+import AddStepScreen, { ADD_STEP_SCREEN } from '../../containers/AddStepScreen';
 import { CELEBRATION_SCREEN } from '../../containers/CelebrationScreen';
 import { GifCompleteFlowScreens } from '../flowCompleted/gifCompleteFlow';
 
 export const AddMyStepFlowScreens = {
-  [SELECT_MY_STEP_SCREEN]: wrapNextScreen(
+  [SELECT_MY_STEP_SCREEN]: wrapNextAction(
     SelectMyStepScreen,
+    ({ receiverId, step }) =>
+      step
+        ? navigatePush(SUGGESTED_STEP_DETAIL_SCREEN, {
+            step,
+            receiverId,
+          })
+        : navigatePush(ADD_STEP_SCREEN, {
+            type: CREATE_STEP,
+            personId: receiverId,
+            trackingObj: buildTrackingObj(
+              'people : self : steps : create',
+              'people',
+              'self',
+              'steps',
+            ),
+          }),
+  ),
+  [SUGGESTED_STEP_DETAIL_SCREEN]: wrapNextScreen(
+    SuggestedStepDetailScreen,
     CELEBRATION_SCREEN,
+  ),
+  [ADD_STEP_SCREEN]: wrapNextAction(
+    AddStepScreen,
+    ({ text, personId, orgId }) => dispatch => {
+      dispatch(createCustomStep(text, personId, orgId));
+      dispatch(navigatePush(CELEBRATION_SCREEN));
+    },
   ),
   ...GifCompleteFlowScreens,
 };
