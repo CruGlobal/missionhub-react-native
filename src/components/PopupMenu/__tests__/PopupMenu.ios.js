5cea9d7c32bb214b378fefc83a846978d886fad8
Robert Eldredge <robert.eldredge@cru.org>
2019-09-09 16:28:21 -0400

    popupMenu iOS button press tests

--- src/components/PopupMenu/__tests__/PopupMenu.ios.js
+++ src/components/PopupMenu/__tests__/PopupMenu.ios.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActionSheetIOS } from 'react-native';
+import { fireEvent } from 'react-native-testing-library';
 
 import PopupMenu from '../index.ios.js';
 import { renderWithContext } from '../../../../testUtils';
 
 const action1 = { text: 'test', onPress: jest.fn() };
-const action2 = { text: 'testing', onPress: jest.fn() };
+const action2 = { text: 'testing', onPress: jest.fn(), destructive: true };
 const actions = [action1, action2];
 const buttonProps = { style: { margin: 10 } };
 const iconProps = { size: 24, style: { paddingVertical: 10 } };
@@ -45,13 +46,66 @@ describe('PopupMenu iOS', () => {
     ).snapshot();
   });
 
-  /*describe('press menu button', () => {
-    it('handles press', () => {
+  describe('press menu button', () => {
+    beforeEach(() => {
+      ActionSheetIOS.showActionSheetWithOptions = jest.fn((a, b) => b(0));
+    });
+
+    it('should call action sheet', () => {
+      const { getByTestId } = renderWithContext(
+        <PopupMenu actions={actions} />,
+      );
+
+      fireEvent(getByTestId('popupMenuButton'), 'onPress');
+
+      expect(ActionSheetIOS.showActionSheetWithOptions).toHaveBeenCalledWith(
+        {
+          cancelButtonIndex: 2,
+          destructiveButtonIndex: 1,
+          options: [action1.text, action2.text, 'Cancel'],
+        },
+        expect.any(Function),
+      );
+      expect(actions[0].onPress).toHaveBeenCalled();
+    });
+
+    it('should call action sheet with title', () => {
+      const title = 'title';
+
+      const { getByTestId } = renderWithContext(
+        <PopupMenu actions={actions} title={title} />,
+      );
+
+      fireEvent(getByTestId('popupMenuButton'), 'onPress');
+
+      expect(ActionSheetIOS.showActionSheetWithOptions).toHaveBeenCalledWith(
+        {
+          cancelButtonIndex: 2,
+          destructiveButtonIndex: 1,
+          options: [action1.text, action2.text, 'Cancel'],
+          title,
+        },
+        expect.any(Function),
+      );
+      expect(actions[0].onPress).toHaveBeenCalled();
+    });
+
+    it('should call action sheet on long press', () => {
       const { getByTestId } = renderWithContext(
         <PopupMenu actions={actions} triggerOnLongPress={true} />,
       );
 
-      fireEvent(getByTestId('popupMenuButton'), 'onPress');
+      fireEvent(getByTestId('popupMenuButton'), 'onLongPress');
+
+      expect(ActionSheetIOS.showActionSheetWithOptions).toHaveBeenCalledWith(
+        {
+          cancelButtonIndex: 2,
+          destructiveButtonIndex: 1,
+          options: [action1.text, action2.text, 'Cancel'],
+        },
+        expect.any(Function),
+      );
+      expect(actions[0].onPress).toHaveBeenCalled();
     });
-  });*/
+  });
 });
