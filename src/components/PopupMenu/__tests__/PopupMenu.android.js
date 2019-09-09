2883a5f21947563236c8afcd926ce145a3beac45
Robert Eldredge <robert.eldredge@cru.org>
2019-09-09 11:33:32 -0400

    remove fireEvent imports

--- src/components/PopupMenu/__tests__/PopupMenu.android.js
+++ src/components/PopupMenu/__tests__/PopupMenu.android.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View } from 'react-native';
-import { fireEvent } from 'react-native-testing-library';
 
 import PopupMenu from '../index.android.js';
 import { renderWithContext } from '../../../../testUtils';
