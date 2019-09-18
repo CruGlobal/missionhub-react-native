30624ebdc06872140c0a7fd6ed88fd213fb48237
Robert Eldredge <35531817+reldredge71@users.noreply.github.com>
2019-09-18 17:43:47 -0400

    MHP 2919 -- Fix for Localized Stages (#1199)
    * fix for contact assignment selector
    
    * store stages in reducer differently
    
    * tests
    
    * remove stageLocale

--- src/actions/stages.js
+++ src/actions/stages.js
@@ -1,3 +1,5 @@
+import i18n from 'i18next';
+
 import { UPDATE_STAGES } from '../constants';
 import { REQUESTS } from '../api/routes';
 
@@ -5,11 +7,15 @@ import callApi from './api';
 
 export function getStagesIfNotExists() {
   return (dispatch, getState) => {
-    const stagesExist = !!getState().stages.stagesObj;
-    if (stagesExist) {
+    const { stages } = getState().stages;
+
+    const localeNotChanged =
+      stages && stages[0] && i18n.language === stages[0].locale;
+
+    if (localeNotChanged) {
       dispatch({
         type: UPDATE_STAGES,
-        stages: getState().stages.stages,
+        stages,
       });
       return Promise.resolve();
     }
