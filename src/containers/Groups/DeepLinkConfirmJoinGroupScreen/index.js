4601b1f9262c08b8b06ca80120d7193284a8fd36
Robert Eldredge <robert.eldredge@cru.org>
2019-10-04 15:23:36 -0400

    Past work for MHP-2643

--- src/containers/Groups/DeepLinkConfirmJoinGroupScreen/index.js
+++ src/containers/Groups/DeepLinkConfirmJoinGroupScreen/index.js
@@ -76,7 +76,34 @@ class DeepLinkConfirmJoinGroupScreen extends Component {
   renderGroupCard() {
     const { community } = this.state;
 
-    return <GroupCardItem group={community} onJoin={this.navigateNext} />;
+    const {
+      id,
+      name,
+      owner,
+      contactReport = {},
+      user_created,
+      community_photo_url,
+      unread_comments_count,
+    } = community;
+
+    const group = {
+      id,
+      name,
+      owner: {
+        firstName: owner.first_name,
+        lastName: owner.last_name,
+      },
+      report: {
+        contactCount: contactReport.contactsCount || 0,
+        unassignedCount: contactReport.unassignedCount || 0,
+        memberCount: contactReport.memberCount || 0,
+      },
+      userCreated: user_created,
+      communityPhotoUrl: community_photo_url,
+      unreadCommentsCount: unread_comments_count,
+    };
+
+    return <GroupCardItem group={group} onJoin={this.navigateNext} />;
   }
 
   render() {
