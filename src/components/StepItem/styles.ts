72e03c7350f537cc89d947ea222c365ab1db9af1
TheNoodleMoose <cjh605@gmail.com>
2019-10-18 16:30:14 -0400

    Change from flex-start to center

--- src/components/StepItem/styles.ts
+++ src/components/StepItem/styles.ts
@@ -49,7 +49,7 @@ export default StyleSheet.create({
     flexDirection: 'column',
   },
   nameWrap: {
-    alignSelf: 'flex-start',
+    alignSelf: 'center',
   },
   description: {
     fontSize: 14,
