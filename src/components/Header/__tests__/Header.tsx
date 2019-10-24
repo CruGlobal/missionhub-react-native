a8b5c39bd456116e1335ed38b3bfa7ad670ae75f
Robert Eldredge <robert.eldredge@cru.org>
2019-10-24 09:35:52 -0400

    Tests

---
+++ src/components/Header/__tests__/Header.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Text } from '../../common';
+import { renderWithContext } from '../../../../testUtils';
+
+import Header from '..';
+
+const bonusContent = <Text>'Bonus'</Text>;
+
+it('renders with no props', () => {
+  renderWithContext(<Header />).snapshot();
+});
+
+it('renders with left content', () => {
+  renderWithContext(<Header left={<Text>Left</Text>} />).snapshot();
+});
+
+it('renders with center content', () => {
+  renderWithContext(<Header center={<Text>Center</Text>} />).snapshot();
+});
+
+it('renders with right content', () => {
+  renderWithContext(<Header right={<Text>Right</Text>} />).snapshot();
+});
+
+it('renders with title', () => {
+  renderWithContext(<Header title="Title" />).snapshot();
+});
+
+it('renders with title and subtitle', () => {
+  renderWithContext(<Header title="Title" title2="Subtitle" />).snapshot();
+});
+
+it('renders with shadow', () => {
+  renderWithContext(<Header shadow={true} />).snapshot();
+});
+
+it('renders with styles', () => {
+  renderWithContext(
+    <Header
+      title="Title"
+      style={{ backgroundColor: 'blue' }}
+      titleStyle={{ color: 'red' }}
+    />,
+  ).snapshot();
+});
