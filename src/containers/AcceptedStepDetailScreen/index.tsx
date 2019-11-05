6e2203a9640580db3f692c7674f93dd42e281971
TheNoodleMoose <cjh605@gmail.com>
2019-11-05 16:40:13 -0500

    Fix lint issue

--- src/containers/AcceptedStepDetailScreen/index.tsx
+++ src/containers/AcceptedStepDetailScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
