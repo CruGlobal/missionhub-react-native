ac7dba61c75466f078301ace8b91dce4e4429807
Scotty Waggoner <ozzieorca@gmail.com>
2019-11-07 19:54:13 -0800

    Fix tests

--- src/routes/addPerson/__tests__/addSomeoneStepFlow.js
+++ src/routes/addPerson/__tests__/addSomeoneStepFlow.js
@@ -187,16 +187,16 @@ describe('PersonSelectStepScreen next', () => {
   });
 
   it('should fire required next actions for suggested step', async () => {
-    await store.dispatch(next({ receiverId: personId, step }));
+    await store.dispatch(next({ personId, step }));
 
     expect(navigatePush).toHaveBeenCalledWith(SUGGESTED_STEP_DETAIL_SCREEN, {
       step,
-      receiverId: personId,
+      personId,
     });
   });
 
   it('should fire required next actions for create step', async () => {
-    await store.dispatch(next({ receiverId: personId, step: undefined }));
+    await store.dispatch(next({ personId, step: undefined }));
 
     expect(navigatePush).toHaveBeenCalledWith(ADD_STEP_SCREEN, {
       type: CREATE_STEP,
@@ -222,7 +222,7 @@ describe('SuggestedStepDetailScreen next', () => {
           state: {
             params: {
               step,
-              receiverId: myId,
+              personId: myId,
             },
           },
         }}
