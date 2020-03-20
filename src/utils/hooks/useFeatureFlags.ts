fef74db5c55989df1a6e914df51623dc2c23f8c4
Robert Eldredge <robert.eldredge@cru.org>
2020-03-20 11:34:39 -0400

    return value from useFeatureFlags

--- src/utils/hooks/useFeatureFlags.ts
+++ src/utils/hooks/useFeatureFlags.ts
@@ -3,10 +3,20 @@ import { useApolloClient } from '@apollo/react-hooks';
 import { GET_FEATURE_FLAGS } from '../../actions/misc';
 import { GetFeatureFlags } from '../../actions/__generated__/GetFeatureFlags';
 
-export const useFeatureFlags = () => {
+export const useFeatureFlags = (
+  flagsToSearchFor: string[],
+): { [key: string]: boolean } => {
   const client = useApolloClient();
 
-  const flags = client.readQuery<GetFeatureFlags>({
+  const { features } = client.readQuery<GetFeatureFlags>({
     query: GET_FEATURE_FLAGS,
-  });
+  }) || { features: [] };
+
+  return flagsToSearchFor.reduce(
+    (acc, flag) => ({
+      ...acc,
+      [flag]: features.includes(flag),
+    }),
+    {},
+  );
 };
