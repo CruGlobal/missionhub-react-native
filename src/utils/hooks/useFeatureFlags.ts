cf808b3bc32e368274f51bdd972c1b86539de47d
Robert Eldredge <robert.eldredge@cru.org>
2020-03-26 15:09:17 -0400

    update hook

--- src/utils/hooks/useFeatureFlags.ts
+++ src/utils/hooks/useFeatureFlags.ts
@@ -1,21 +1,20 @@
-import { useApolloClient } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 
 import { GET_FEATURE_FLAGS } from '../../actions/misc';
 import { GetFeatureFlags } from '../../actions/__generated__/GetFeatureFlags';
 
-export const useFeatureFlags = (
-  flagsToSearchFor: string[],
-): { [key: string]: boolean } => {
-  const client = useApolloClient();
+export const useFeatureFlags = (): { [key: string]: boolean } => {
+  const { data: { features = [] } = {} } = useQuery<GetFeatureFlags>(
+    GET_FEATURE_FLAGS,
+    {
+      fetchPolicy: 'cache-and-network',
+    },
+  );
 
-  const { features } = client.readQuery<GetFeatureFlags>({
-    query: GET_FEATURE_FLAGS,
-  }) || { features: [] };
-
-  return flagsToSearchFor.reduce(
+  return features.reduce(
     (acc, flag) => ({
       ...acc,
-      [flag]: features.includes(flag),
+      [flag]: true,
     }),
     {},
   );
