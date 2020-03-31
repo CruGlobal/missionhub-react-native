744fe5aefffe1466749000d8180c99fd266c9bdb
Robert Eldredge <robert.eldredge@cru.org>
2020-03-31 16:41:13 -0400

    revert MHP-3082

--- src/containers/WelcomeScreen/index.tsx
+++ src/containers/WelcomeScreen/index.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
 import { connect } from 'react-redux-legacy';
-import { useDispatch } from 'react-redux';
 import { View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useNavigationParam } from 'react-navigation-hooks';
-import { ThunkAction } from 'redux-thunk';
+import { ThunkDispatch, ThunkAction } from 'redux-thunk';
 
-import { Text, Button } from '../../components/common';
+import { Flex, Text, Button } from '../../components/common';
 import BottomButton from '../../components/BottomButton';
 import {
   trackActionWithoutData,
@@ -23,18 +21,22 @@ import { OnboardingState } from '../../reducers/onboarding';
 import styles from './styles';
 
 interface WelcomeScreenProps {
+  dispatch: ThunkDispatch<{}, null, never>;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   next: (params: { signin: boolean }) => ThunkAction<void, any, null, never>;
   analyticsSection: TrackStateContext[typeof ANALYTICS_SECTION_TYPE];
 }
 
-const WelcomeScreen = ({ next, analyticsSection }: WelcomeScreenProps) => {
+const WelcomeScreen = ({
+  dispatch,
+  next,
+  analyticsSection,
+}: WelcomeScreenProps) => {
   useAnalytics(['onboarding', 'welcome'], {
     screenContext: {
       [ANALYTICS_SECTION_TYPE]: analyticsSection,
     },
   });
-  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(trackActionWithoutData(ACTIONS.ONBOARDING_STARTED));
@@ -51,45 +53,44 @@ const WelcomeScreen = ({ next, analyticsSection }: WelcomeScreenProps) => {
   const allowSignIn = useNavigationParam('allowSignIn');
   const { t } = useTranslation('welcome');
 
-  const signInButtons = () => (
-    <SafeAreaView style={styles.signInWrapper}>
-      <Button
-        testID="sign-in"
-        pill={true}
-        onPress={signIn}
-        style={styles.filledButton}
-        buttonTextStyle={styles.buttonText}
-        text={t('signIn').toUpperCase()}
-      />
-      <Button
-        testID="get-started-sign-in-variant"
-        pill={true}
-        onPress={navigateToNext}
-        style={styles.clearButton}
-        buttonTextStyle={styles.buttonText}
-        text={t('getStartedButton').toUpperCase()}
-      />
-    </SafeAreaView>
-  );
-
   return (
     <View style={styles.container}>
       <Header left={<BackButton />} />
-      <View style={styles.content}>
-        <Text header={true} style={styles.headerText}>
-          {t('welcome')}
-        </Text>
-        <Text style={styles.descriptionText}>{t('welcomeDescription')}</Text>
-      </View>
-      {allowSignIn ? (
-        signInButtons()
-      ) : (
-        <BottomButton
-          testID={'get-started'}
-          onPress={navigateToNext}
-          text={t('continue')}
-        />
-      )}
+      <Flex align="center" justify="center" value={1} style={styles.content}>
+        <Flex value={3} align="start" justify="center">
+          <Text header={true} style={styles.headerText}>
+            {t('welcome')}
+          </Text>
+          <Text style={styles.descriptionText}>{t('welcomeDescription')}</Text>
+        </Flex>
+
+        {allowSignIn ? (
+          <Flex value={1} align="center" justify="start">
+            <Button
+              testID="sign-in"
+              pill={true}
+              onPress={signIn}
+              style={styles.filledButton}
+              buttonTextStyle={styles.buttonText}
+              text={t('signIn').toUpperCase()}
+            />
+            <Button
+              testID="get-started-sign-in-variant"
+              pill={true}
+              onPress={navigateToNext}
+              style={styles.clearButton}
+              buttonTextStyle={styles.buttonText}
+              text={t('getStartedButton').toUpperCase()}
+            />
+          </Flex>
+        ) : (
+          <BottomButton
+            testID={'get-started'}
+            onPress={navigateToNext}
+            text={t('continue')}
+          />
+        )}
+      </Flex>
     </View>
   );
 };
