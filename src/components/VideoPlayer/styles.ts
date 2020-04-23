e3da63f805d78275235a576fbc19264535c3df52
TheNoodleMoose <cjh605@gmail.com>
2020-04-23 16:53:02 -0400

    Add basic video player component from react-native-video

---
+++ src/components/VideoPlayer/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+// import theme from '../../theme';
+
+export default StyleSheet.create({
+  videoContainer: {
+    width: '100%',
+    height: 300,
+  },
+  videoPlayer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
