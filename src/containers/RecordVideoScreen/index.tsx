0232823045838e9f55bb11ab6ea42cb7ea64a823
Robert Eldredge <robert.eldredge@cru.org>
2020-05-07 16:12:22 -0400

    finish record screen layout

--- src/containers/RecordVideoScreen/index.tsx
+++ src/containers/RecordVideoScreen/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
+import { View, SafeAreaView } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
-import { Text, Button } from '../../components/common';
+import { Text, Button, Touchable } from '../../components/common';
 import CloseButton from '../../../assets/images/closeIcon.svg';
 import theme from '../../theme';
+
+import CameraRotateIcon from './cameraRotateIcon.svg';
+import styles from './styles';
 
 enum VideoState {
   NOT_RECORDING,
@@ -23,71 +25,56 @@ export const RecordVideoScreen = () => {
 
   const endRecording = () => {};
 
-  const handleBuffer = () => {};
-
-  const handleError = (error: Error) => {
-    return error;
-  };
-
   const handleClose = () => {};
 
+  const handleFlipCamera = () => {};
+
   const renderCloseButton = () => (
-    <SafeAreaView
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        padding: 18,
-      }}
-    >
+    <SafeAreaView style={styles.closeButtonWrap}>
       <Button
         onPress={handleClose}
         type="transparent"
-        style={{ backgroundColor: theme.black, borderRadius: 18, opacity: 0.5 }}
+        style={styles.closeButton}
       >
         <CloseButton color={theme.white} height={36} width={36} />
       </Button>
     </SafeAreaView>
   );
 
-  const renderRecordButton = () =>
-    videoState === VideoState.NOT_RECORDING ? (
-      <Button
-        type="transparent"
-        onPress={startRecording}
-        style={{ backgroundColor: theme.parakeetBlue }}
-        text={'START'}
-      />
-    ) : (
-      <Button
-        onPress={endRecording}
-        pill={true}
-        style={{ backgroundColor: theme.red }}
-        text={'STOP'}
-      />
-    );
+  const renderRecordButton = () => (
+    <Touchable
+      style={styles.recordButton}
+      onPress={
+        videoState === VideoState.NOT_RECORDING ? startRecording : endRecording
+      }
+    >
+      {videoState === VideoState.NOT_RECORDING ? (
+        <View style={styles.startRecordIcon} />
+      ) : (
+        <View style={styles.endRecordIcon} />
+      )}
+    </Touchable>
+  );
 
   const renderControlBar = () => (
     <View>
-      <View
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          backgroundColor: 'black',
-          opacity: 0.5,
-        }}
-      />
-      <SafeAreaView style={{ flexDirection: 'row', padding: 24 }}>
-        <Text>{`:${Math.ceil(countDownTime / 1000)}`}</Text>
-        {renderRecordButton()}
+      <View style={styles.controlBarBackground} />
+      <SafeAreaView>
+        <View style={styles.controlBarContainer}>
+          <Text style={styles.countdownText}>{`:${Math.ceil(
+            countDownTime / 1000,
+          )}`}</Text>
+          {renderRecordButton()}
+          <Touchable onPress={handleFlipCamera}>
+            <CameraRotateIcon />
+          </Touchable>
+        </View>
       </SafeAreaView>
     </View>
   );
 
   return (
-    <View style={{ flex: 1, justifyContent: 'space-between' }}>
+    <View style={styles.container}>
       {renderCloseButton()}
       {renderControlBar()}
     </View>
---
+++ src/containers/RecordVideoScreen/styles.ts
@@ -0,0 +1,61 @@
+import { StyleSheet } from 'react-native';
+
+import theme from '../../theme';
+
+export default StyleSheet.create({
+  container: { flex: 1, justifyContent: 'space-between' },
+  controlBarBackground: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'black',
+    opacity: 0.5,
+  },
+  controlBarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 32,
+  },
+  countdownText: {
+    fontWeight: '300',
+    color: theme.white,
+    fontSize: 24,
+    lineHeight: 30,
+    textAlign: 'center',
+  },
+  recordButton: {
+    height: 48,
+    width: 48,
+    borderWidth: 2,
+    borderColor: theme.white,
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  startRecordIcon: {
+    backgroundColor: theme.red,
+    height: 24,
+    width: 24,
+    borderRadius: 12,
+  },
+  endRecordIcon: {
+    backgroundColor: theme.red,
+    height: 20,
+    width: 20,
+    borderRadius: 2,
+  },
+  closeButtonWrap: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  closeButton: {
+    backgroundColor: theme.black,
+    borderRadius: 18,
+    opacity: 0.5,
+    margin: 18,
+  },
+});
