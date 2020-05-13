6c27af847557eb8f6bdeb4693a2e9101c18ffd90
Scotty Waggoner <ozzieorca@gmail.com>
2020-05-12 20:11:27 -0700

    MHP-3232: Community header styling fixes

--- src/components/HeaderTabBar/__tests__/HeaderTabBar.tsx
+++ src/components/HeaderTabBar/__tests__/HeaderTabBar.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { fireEvent } from 'react-native-testing-library';
 // eslint-disable-next-line import/named
 import { NavigationActions } from 'react-navigation';
+import { useNavigationState } from 'react-navigation-hooks';
 
 import { renderWithContext } from '../../../../testUtils';
 import { HeaderTabBar } from '../HeaderTabBar';
 import { Touchable } from '../../common';
+
+jest.mock('react-navigation-hooks');
 
 // @ts-ignore
 NavigationActions.navigate = jest.fn(() => ({ type: 'navigate' }));
@@ -17,14 +20,28 @@ const tabs = [
 
 describe('HeaderTabBar', () => {
   it('should render tabs', () => {
+    (useNavigationState as jest.Mock).mockReturnValue({ index: 0 });
+    renderWithContext(<HeaderTabBar tabs={tabs} />).snapshot();
+  });
+
+  it('should render active tab', () => {
+    (useNavigationState as jest.Mock).mockReturnValue({ index: 1 });
     renderWithContext(<HeaderTabBar tabs={tabs} />).snapshot();
   });
 
   it('should change tabs', () => {
+    (useNavigationState as jest.Mock).mockReturnValue({ index: 0 });
     const { getAllByType } = renderWithContext(<HeaderTabBar tabs={tabs} />);
     fireEvent.press(getAllByType(Touchable)[1]);
     expect(NavigationActions.navigate).toHaveBeenCalledWith({
       routeName: 'Screen Two',
     });
+  });
+
+  it('should not change tabs if the tab is already selected', () => {
+    (useNavigationState as jest.Mock).mockReturnValue({ index: 1 });
+    const { getAllByType } = renderWithContext(<HeaderTabBar tabs={tabs} />);
+    fireEvent.press(getAllByType(Touchable)[1]);
+    expect(NavigationActions.navigate).not.toHaveBeenCalled();
   });
 });
