83a66e2f4a6e90d933449dda11a867351a7cf72c
Scotty Waggoner <ozzieorca@gmail.com>
2020-05-19 13:36:18 -0700

    Add support for FeedItemComment in content report screens

--- src/containers/CommentItem/index.tsx
+++ src/containers/CommentItem/index.tsx
@@ -48,14 +48,14 @@ const CommentItem = ({
     editingStyle,
     name: nameStyle,
   } = styles;
-  const isMine = person ? person.id === me.id : author.id === me.id;
+  const isMine = person?.id === me.id || author?.id === me.id;
   const isMineNotReported = isMine && !isReported;
   const itemDate = created_at ? created_at : createdAt ? createdAt : '';
   const name = person
     ? person.first_name
       ? `${person.first_name} ${person.last_name}`
       : person.fullName
-    : author.fullName;
+    : author?.fullName;
 
   const renderContent = () => {
     return (
