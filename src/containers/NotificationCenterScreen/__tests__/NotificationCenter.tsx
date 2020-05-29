5ae28ad5e90c869eccbf69824534f40c5de6be82
TheNoodleMoose <cjh605@gmail.com>
2020-05-29 11:30:35 -0400

    Add test for different sections

--- src/containers/NotificationCenterScreen/__tests__/NotificationCenter.tsx
+++ src/containers/NotificationCenterScreen/__tests__/NotificationCenter.tsx
@@ -3,6 +3,7 @@ import { SectionList } from 'react-native';
 import { flushMicrotasksQueue, fireEvent } from 'react-native-testing-library';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { MockList } from 'graphql-tools';
+import MockDate from 'mockdate';
 
 import { renderWithContext } from '../../../../testUtils';
 import { GET_NOTIFICATIONS, UPDATE_HAS_UNREAD_NOTIFICATIONS } from '../queries';
@@ -12,6 +13,9 @@ import NotificationCenterScreen from '..';
 
 jest.mock('../../../utils/common');
 
+const mockDate = '2020-05-20 12:00:00 PM GMT+0';
+
+MockDate.set(mockDate);
 const openMainMenuResponse = { type: 'open main menu' };
 
 const initialApolloState = {
@@ -79,4 +83,48 @@ it('handles refresh', async () => {
   await flushMicrotasksQueue();
   fireEvent(getByType(SectionList), 'onRefresh');
   expect(useMutation).toHaveBeenCalledWith(UPDATE_HAS_UNREAD_NOTIFICATIONS);
+});
+
+it('renders Today section correctly ', async () => {
+  const { snapshot } = renderWithContext(<NotificationCenterScreen />, {
+    mocks: {
+      NotificationConnection: () => ({
+        nodes: () =>
+          new MockList(1, () => ({
+            createdAt: '2020-05-20 11:00:00 AM GMT+0',
+          })),
+      }),
+    },
+    initialApolloState,
+  });
+
+  await flushMicrotasksQueue();
+  expect(useQuery).toHaveBeenCalledWith(GET_NOTIFICATIONS, {
+    pollInterval: 30000,
+  });
+  expect(useMutation).toHaveBeenCalledWith(UPDATE_HAS_UNREAD_NOTIFICATIONS);
+
+  snapshot();
+});
+
+it('renders Earlier section correctly ', async () => {
+  const { snapshot } = renderWithContext(<NotificationCenterScreen />, {
+    mocks: {
+      NotificationConnection: () => ({
+        nodes: () =>
+          new MockList(1, () => ({
+            createdAt: '2020-05-18 12:00:00 PM GMT+0',
+          })),
+      }),
+    },
+    initialApolloState,
+  });
+
+  await flushMicrotasksQueue();
+  expect(useQuery).toHaveBeenCalledWith(GET_NOTIFICATIONS, {
+    pollInterval: 30000,
+  });
+  expect(useMutation).toHaveBeenCalledWith(UPDATE_HAS_UNREAD_NOTIFICATIONS);
+
+  snapshot();
 });
