a70b4cc93ef290c44abe4f63bdb38d7b424548b9
Robert Eldredge <robert.eldredge@cru.org>
2020-06-03 13:18:39 -0400

    start work on new analytics utils

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -5,40 +5,20 @@ import {
   ANALYTICS_EDIT_MODE,
 } from '../constants';
 import { TrackStateContext } from '../actions/analytics';
-import { AuthState } from '../reducers/auth';
 import { OnboardingState } from '../reducers/onboarding';
-import { orgPermissionSelector } from '../selectors/people';
 import { PermissionEnum } from '../../__generated__/globalTypes';
+import { apolloClient } from '../apolloClient';
+import { GET_MY_COMMUNITY_PERMISSION_QUERY } from '../containers/Groups/CreatePostButton/queries';
+import { getMyCommunityPermission } from '../containers/Groups/CreatePostButton/__generated__/getMyCommunityPermission';
 
-import {
-  personIsCurrentUser,
-  hasOrgPermissions,
-  isOnboarding,
-  isOwner,
-  isAdmin,
-} from './common';
+import { isOnboarding } from './common';
 
 export const getAnalyticsAssignmentType = (
-  person: {
-    id: string;
-    organizational_permissions?: {
-      organization_id: string;
-      permission_id: string;
-    }[];
-  } | null,
-  authState: AuthState,
-  organization?: { id: string },
-): TrackStateContext[typeof ANALYTICS_ASSIGNMENT_TYPE] => {
-  const orgPermission = orgPermissionSelector({}, { person, organization });
-
-  return person
-    ? personIsCurrentUser(person.id, authState)
-      ? 'self'
-      : orgPermission && hasOrgPermissions(orgPermission)
-      ? 'community member'
-      : 'contact'
-    : '';
-};
+  personId: string,
+  myId: string,
+  communityId?: string,
+): TrackStateContext[typeof ANALYTICS_ASSIGNMENT_TYPE] =>
+  personId === myId ? 'self' : communityId ? 'community member' : 'contact';
 
 export const getAnalyticsSectionType = (
   onboardingState: OnboardingState,
@@ -50,24 +30,6 @@ export const getAnalyticsEditMode = (
 ): TrackStateContext[typeof ANALYTICS_EDIT_MODE] => (isEdit ? 'update' : 'set');
 
 export const getAnalyticsPermissionType = (
-  auth: AuthState,
-  organization: { id: string },
-): TrackStateContext[typeof ANALYTICS_PERMISSION_TYPE] => {
-  const orgPermission = orgPermissionSelector(
-    {},
-    { person: auth.person, organization },
-  );
-
-  return hasOrgPermissions(orgPermission)
-    ? isOwner(orgPermission)
-      ? 'owner'
-      : isAdmin(orgPermission)
-      ? 'admin'
-      : 'member'
-    : '';
-};
-
-export const getAnalyticsPermissionTypeGraphQL = (
   permission?: PermissionEnum,
 ): TrackStateContext[typeof ANALYTICS_PERMISSION_TYPE] => {
   switch (permission) {
--- src/utils/hooks/useAnalytics.ts
+++ src/utils/hooks/useAnalytics.ts
@@ -1,9 +1,18 @@
 import { useEffect } from 'react';
 import { useIsFocused } from 'react-navigation-hooks';
 import { useDispatch, useSelector } from 'react-redux';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
 import { trackScreenChange, ScreenContext } from '../../actions/analytics';
 import { DrawerState } from '../../reducers/drawer';
+import { apolloClient } from '../../apolloClient';
+import { GET_MY_COMMUNITY_PERMISSION_QUERY } from '../../containers/Groups/CreatePostButton/queries';
+import { getMyCommunityPermission } from '../../containers/Groups/CreatePostButton/__generated__/getMyCommunityPermission';
+import {
+  getAnalyticsPermissionType,
+  getAnalyticsAssignmentType,
+} from '../analytics';
 
 export enum ANALYTICS_SCREEN_TYPES {
   screen,
@@ -16,13 +25,44 @@ interface UseAnalyticsOptions {
   screenContext?: Partial<ScreenContext>;
 }
 
-export const useAnalytics = (
+export const useAnalytics = async (
   screenName: string | string[],
   {
     screenType = ANALYTICS_SCREEN_TYPES.screen,
     screenContext,
   }: UseAnalyticsOptions = {},
 ) => {
+  const communityId = '1111';
+  const personId = '2222';
+
+  const {} = apolloClient.readFragment({
+    fragment: gql`
+    fragment stepReceiver on Step {
+      receiver {
+        id
+      }
+  `,
+  });
+
+  const {
+    data: { community: { people: { edges = [] } = {} } = {} } = {},
+  } = useQuery<getMyCommunityPermission>(GET_MY_COMMUNITY_PERMISSION_QUERY, {
+    variables: {
+      communityId,
+      personId,
+    },
+    fetchPolicy: 'cache-first',
+  });
+
+  const assignmentType = getAnalyticsAssignmentType(
+    personId,
+    myId,
+    communityId,
+  );
+  const permissionType = getAnalyticsPermissionType(
+    edges[0].communityPermission.permission,
+  );
+
   const dispatch = useDispatch();
   const isFocused = useIsFocused();
   const isDrawerOpen = useSelector(
