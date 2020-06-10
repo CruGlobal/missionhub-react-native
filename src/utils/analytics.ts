eadd9bfcd2f19ba45c03104941899410c07f8d43
Robert Eldredge <robert.eldredge@cru.org>
2020-06-10 13:15:35 -0400

    update analytics utils

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -5,75 +5,31 @@ import {
   ANALYTICS_EDIT_MODE,
 } from '../constants';
 import { TrackStateContext } from '../actions/analytics';
-import { AuthState } from '../reducers/auth';
-import { OnboardingState } from '../reducers/onboarding';
-import { orgPermissionSelector } from '../selectors/people';
 import { PermissionEnum } from '../../__generated__/globalTypes';
 
-import {
-  personIsCurrentUser,
-  hasOrgPermissions,
-  isOnboarding,
-  isOwner,
-  isAdmin,
-} from './common';
-
 export const getAnalyticsAssignmentType = (
-  person: {
-    id: string;
-    organizational_permissions?: {
-      organization_id: string;
-      permission_id: string;
-    }[];
-  } | null,
-  authState: AuthState,
-  organization?: { id: string },
-): TrackStateContext[typeof ANALYTICS_ASSIGNMENT_TYPE] => {
-  const orgPermission = orgPermissionSelector({}, { person, organization });
-
-  return person
-    ? personIsCurrentUser(person.id, authState)
-      ? 'self'
-      : orgPermission && hasOrgPermissions(orgPermission)
-      ? 'community member'
-      : 'contact'
-    : '';
-};
+  isMe: boolean,
+  isInCommunity: boolean,
+): TrackStateContext[typeof ANALYTICS_ASSIGNMENT_TYPE] =>
+  isMe ? 'self' : isInCommunity ? 'community member' : 'contact';
 
 export const getAnalyticsSectionType = (
-  onboardingState: OnboardingState,
+  isOnboarding: boolean,
 ): TrackStateContext[typeof ANALYTICS_SECTION_TYPE] =>
-  isOnboarding(onboardingState) ? 'onboarding' : '';
+  isOnboarding ? 'onboarding' : '';
 
 export const getAnalyticsEditMode = (
   isEdit: boolean,
 ): TrackStateContext[typeof ANALYTICS_EDIT_MODE] => (isEdit ? 'update' : 'set');
 
 export const getAnalyticsPermissionType = (
-  auth: AuthState,
-  organization: { id: string },
-): TrackStateContext[typeof ANALYTICS_PERMISSION_TYPE] => {
-  const orgPermission = orgPermissionSelector(
-    {},
-    { person: auth.person, organization },
-  );
-
-  return hasOrgPermissions(orgPermission)
-    ? isOwner(orgPermission)
-      ? 'owner'
-      : isAdmin(orgPermission)
-      ? 'admin'
-      : 'member'
-    : '';
-};
-
-export const getAnalyticsPermissionTypeGraphQL = (
   permission?: PermissionEnum,
 ): TrackStateContext[typeof ANALYTICS_PERMISSION_TYPE] => {
   switch (permission) {
     case PermissionEnum.owner:
+      return 'owner';
     case PermissionEnum.admin:
-      return permission;
+      return 'admin';
     case PermissionEnum.user:
       return 'member';
     default:
--- src/utils/hooks/useAnalytics.ts
+++ src/utils/hooks/useAnalytics.ts
@@ -1,9 +1,27 @@
 import { useEffect } from 'react';
 import { useIsFocused } from 'react-navigation-hooks';
 import { useDispatch, useSelector } from 'react-redux';
+import { useQuery } from '@apollo/react-hooks';
 
 import { trackScreenChange, ScreenContext } from '../../actions/analytics';
 import { DrawerState } from '../../reducers/drawer';
+import { GET_MY_COMMUNITY_PERMISSION_QUERY } from '../../containers/Groups/CreatePostButton/queries';
+import { getMyCommunityPermission } from '../../containers/Groups/CreatePostButton/__generated__/getMyCommunityPermission';
+import {
+  getAnalyticsPermissionType,
+  getAnalyticsAssignmentType,
+  getAnalyticsSectionType,
+  getAnalyticsEditMode,
+} from '../analytics';
+import {
+  ANALYTICS_ASSIGNMENT_TYPE,
+  ANALYTICS_SECTION_TYPE,
+  ANALYTICS_EDIT_MODE,
+  ANALYTICS_PERMISSION_TYPE,
+} from '../../constants';
+
+import { useIsMe, useMyId } from './useIsMe';
+import { useIsOnboarding } from './useIsOnboarding';
 
 export enum ANALYTICS_SCREEN_TYPES {
   screen,
@@ -11,18 +29,76 @@ export enum ANALYTICS_SCREEN_TYPES {
   drawer,
 }
 
-interface UseAnalyticsOptions {
+export interface UseAnalyticsParams {
+  communityId?: string;
+  personId?: string;
+  isEdit?: boolean;
+}
+
+export interface UseAnalyticsOptions {
   screenType?: ANALYTICS_SCREEN_TYPES;
-  screenContext?: Partial<ScreenContext>;
+  includeAssignmentType?: boolean;
+  includeSectionType?: boolean;
+  includeEditMode?: boolean;
+  includePermissionType?: boolean;
 }
 
 export const useAnalytics = (
   screenName: string | string[],
+  { communityId = '', personId = '', isEdit = false }: UseAnalyticsParams = {},
   {
     screenType = ANALYTICS_SCREEN_TYPES.screen,
-    screenContext,
+    includeAssignmentType,
+    includeSectionType,
+    includeEditMode,
+    includePermissionType,
   }: UseAnalyticsOptions = {},
 ) => {
+  const myId = useMyId();
+  const isMe = useIsMe(personId);
+  const isOnboarding = useIsOnboarding();
+
+  const {
+    data: { community: { people: { edges = [] } = {} } = {} } = {},
+  } = useQuery<getMyCommunityPermission>(GET_MY_COMMUNITY_PERMISSION_QUERY, {
+    variables: {
+      id: communityId,
+      myId,
+    },
+    fetchPolicy: 'cache-first',
+  });
+
+  let screenContext: Partial<ScreenContext> = {};
+  if (includeAssignmentType) {
+    screenContext = {
+      ...screenContext,
+      [ANALYTICS_ASSIGNMENT_TYPE]: getAnalyticsAssignmentType(
+        isMe,
+        !!communityId,
+      ),
+    };
+  }
+  if (includeSectionType) {
+    screenContext = {
+      ...screenContext,
+      [ANALYTICS_SECTION_TYPE]: getAnalyticsSectionType(isOnboarding),
+    };
+  }
+  if (includeEditMode) {
+    screenContext = {
+      ...screenContext,
+      [ANALYTICS_EDIT_MODE]: getAnalyticsEditMode(isEdit),
+    };
+  }
+  if (includePermissionType) {
+    screenContext = {
+      ...screenContext,
+      [ANALYTICS_PERMISSION_TYPE]: getAnalyticsPermissionType(
+        edges[0].communityPermission.permission,
+      ),
+    };
+  }
+
   const dispatch = useDispatch();
   const isFocused = useIsFocused();
   const isDrawerOpen = useSelector(
