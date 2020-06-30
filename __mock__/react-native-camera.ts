4765ac7b69fdae2dc09ffdd4bfb299598ddae93a
Robert Eldredge <robert.eldredge@cru.org>
2020-06-30 16:46:41 -0400

    Trying to get the mocks to work

--- __mock__/react-native-camera.ts
+++ __mock__/react-native-camera.ts
@@ -1,42 +1,42 @@
-import React from 'react';
+/*import React from 'react';
+
+const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export class RNCamera extends React.Component {
+  static Constants = {
+    Aspect: {},
+    BarCodeType: {},
+    Type: { back: 'back', front: 'front' },
+    CaptureMode: {},
+    CaptureTarget: {},
+    CaptureQuality: {},
+    Orientation: {},
+    FlashMode: {},
+    TorchMode: {},
+  };
+
+  takePictureAsync = async () => {
+    await timeout(2000);
+    return {
+      base64: 'base64string',
+    };
+  };
+
+  recordAsync = async () => {
+    await timeout(2000);
+    return {
+      uri: 'file:/video.mov',
+    };
+  };
+
+  stopRecording = () => {};
+
+  render() {
+    return 'Camera';
+  }
+}*/
 
 jest.mock('react-native-camera', () => {
-  const timeout = (ms: number) =>
-    new Promise(resolve => setTimeout(resolve, ms));
-
-  class RNCamera extends React.Component {
-    static Constants = {
-      Aspect: {},
-      BarCodeType: {},
-      Type: { back: 'back', front: 'front' },
-      CaptureMode: {},
-      CaptureTarget: {},
-      CaptureQuality: {},
-      Orientation: {},
-      FlashMode: {},
-      TorchMode: {},
-    };
-
-    takePictureAsync = async () => {
-      await timeout(2000);
-      return {
-        base64: 'base64string',
-      };
-    };
-
-    recordAsync = async () => {
-      await timeout(2000);
-      return {
-        uri: 'file:/video.mov',
-      };
-    };
-
-    stopRecording = () => {};
-
-    render() {
-      return 'Camera';
-    }
-  }
-
-  return { RNCamera };
+  const wrapper = () => 'Wrapper';
+  const RNCamera = children => <Wrapper>{children}</Wrapper>;
 });
