19d4e5a48e69b191ab987eef3ddd18d46bc47db0
Robert Eldredge <robert.eldredge@cru.org>
2020-07-06 15:14:08 -0400

    other fixes

--- android/app/src/main/java/com/missionhub/MainApplication.java
+++ android/app/src/main/java/com/missionhub/MainApplication.java
@@ -8,8 +8,6 @@ import android.content.Context;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
-import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
