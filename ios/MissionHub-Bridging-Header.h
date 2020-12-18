eb25783d7e8452fdd1357af27c1beca3b5c694af
Christian Huffman <39680460+TheNoodleMoose@users.noreply.github.com>
2020-12-18 13:59:11 -0500

    Revert "Add changes to enable debug for ios QA"

--- ios/MissionHub-Bridging-Header.h
+++ ios/MissionHub-Bridging-Header.h
@@ -2,10 +2,3 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-// The Google/Analytics pod has been deprecated. Since moving to the GoogleAnalytics pod, these need to be manually added to the bridging header.
-#import <GoogleAnalytics/GAI.h>
-#import <GoogleAnalytics/GAIFields.h>
-#import <GoogleAnalytics/GAILogger.h>
-#import <GoogleAnalytics/GAIDictionaryBuilder.h>
-#import <GoogleAnalytics/GAITrackedViewController.h>
-#import <GoogleAnalytics/GAITracker.h>
